d73ea1ac77f2bf2fe51040b5d936f0138684d8e4
Max Albrecht <max.albrecht@zhdk.ch>
2018-04-03 18:08:39 +0200

    add graphql client, test connection
    https://www.apollographql.com/docs/react/essentials/get-started.html#creating-client

--- src/index.js
+++ src/index.js
@@ -6,5 +6,24 @@ import './styles.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
+import ApolloClient from 'apollo-boost'
+import gql from 'graphql-tag'
+
+const client = new ApolloClient({
+  uri: '/graphql'
+})
+
+client
+  .query({
+    query: gql`
+      {
+        requests {
+          id
+        }
+      }
+    `
+  })
+  .then(data => console.log({ data })) // eslint-disable-line no-console
+
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
