6270dccbcf880cfc81ebe12633e146a066f0c64f
Max Albrecht <max.albrecht@zhdk.ch>
2018-04-04 17:28:30 +0200

    poc filtered query for requests index

--- src/RequestsIndex.js
+++ src/RequestsIndex.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { Fragment as F } from 'react'
+import f from 'lodash'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
@@ -15,7 +16,7 @@ const Loading = () => (
 )
 
 const RequestLine = ({ fields }) => (
-  <React.Fragment>
+  <F>
     <Row>
       <div className="col-sm-2">{fields.article_name}</div>
       <div className="col-sm-2">{fields.receiver_name}</div>
@@ -53,38 +54,88 @@ const RequestLine = ({ fields }) => (
     </Row>
     {/* <pre>{JSON.stringify({ fields }, 0, 2)}</pre> */}
     <hr />
-  </React.Fragment>
+  </F>
 )
 
-const FilterBar = () => (
+const FilterBar = ({ filters, onFilterChange }) => (
   <Div cls="pt-2">
-    <code>TODO</code>
+    <h5>Filters</h5>
+    <fieldset>
+      <legend className="h6">Budgetperioden</legend>
+      {f.sortBy(filters.available.budgetPeriods, 'name').map(({ id, name }) => (
+        <F key={id}>
+          <label>
+            <input
+              type="radio"
+              name="budgetPeriods"
+              value={id}
+              onChange={() => onFilterChange({ budgetPeriods: [id] })}
+            />
+            {name}
+          </label>
+          <br />
+        </F>
+      ))}
+    </fieldset>
   </Div>
 )
-const RequestsList = () => {
-  const query = gql`
-    {
-      requests {
+
+const RequestsIndex = ({ requests, filters, onFilterChange }) => (
+  <MainWithSidebar
+    sidebar={<FilterBar filters={filters} onFilterChange={onFilterChange} />}>
+    <h4>{requests.length} Requests</h4>
+    {requests.map(r => <RequestLine key={r.id} fields={r} />)}
+  </MainWithSidebar>
+)
+
+class RequestsIndexWithData extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      currentFilters: {
+        budgetPeriods: ['2292d02b-44cc-4342-8c76-0cc29ff7a92b']
+      }
+    }
+    this.onFilterChange = this.onFilterChange.bind(this)
+  }
+  onFilterChange(filters) {
+    this.setState({
+      currentFilters: { ...this.state.currentFilters, ...filters }
+    })
+  }
+  render({ state } = this) {
+    const filters = state.currentFilters
+    // FIXME: use real variables, not String interpolation!
+    const query = gql`
+    query RequestsIndex($budgetPeriods: [ID]) {
+      # for filterbar:
+      budget_periods {
+        id
+        name
+      }
+      # main index:
+      requests(budget_period_id: ${JSON.stringify(filters.budgetPeriods)}) {
         ...RequestFieldsForIndex
       }
     }
     ${Fragments.RequestFieldsForIndex}
   `
 
-  const render = ({ loading, error, data }) => {
-    if (loading) return <Loading />
-    if (error) return <p>Error :(</p>
+    const render = ({ loading, error, data }) => {
+      if (loading) return <Loading />
+      if (error) return <p>Error :(</p>
 
-    return data.requests.map(r => <RequestLine key={r.id} fields={r} />)
+      return (
+        <RequestsIndex
+          requests={data.requests}
+          filters={{ available: { budgetPeriods: data.budget_periods } }}
+          onFilterChange={this.onFilterChange}
+        />
+      )
+    }
+
+    return <Query query={query}>{render}</Query>
   }
-
-  return <Query query={query}>{render}</Query>
 }
 
-const RequestsIndex = () => (
-  <MainWithSidebar sidebar={<FilterBar />}>
-    <RequestsList />
-  </MainWithSidebar>
-)
-
-export default RequestsIndex
+export default RequestsIndexWithData
