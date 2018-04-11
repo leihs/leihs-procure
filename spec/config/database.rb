e6de4ba4340dae83437decea05216d8991bf61ef
Matúš Kmiť <matus.kmit@zhdk.ch>
2018-04-11 16:53:45 +0200

    fix rspec database

--- spec/config/database.rb
+++ spec/config/database.rb
@@ -1,7 +1,21 @@
 require 'sequel'
 
 def database
-  Sequel.postgres(host: 'localhost', database: 'leihs_test')
+  Sequel.connect(
+    if (db_env = ENV['LEIHS_DATABASE_URL'].presence)
+      # trick Addressable to parse db urls
+      http_uri = Addressable::URI.parse db_env.gsub(/^jdbc:postgresql/,'http').gsub(/^postgres/,'http')
+      db_url = 'postgres://' \
+        + (http_uri.user.presence || ENV['PGUSER'].presence || 'postgres') \
+        + ((pw = (http_uri.password.presence || ENV['PGPASSWORD'].presence)) ? ":#{pw}" : "") \
+        + '@' + (http_uri.host.presence || ENV['PGHOST'].presence || ENV['PGHOSTADDR'].presence || 'localhost') \
+        + ':' + (http_uri.port.presence || ENV['PGPORT'].presence || 5432).to_s \
+        + '/' + ( http_uri.path.presence.try(:gsub,/^\//,'') || ENV['PGDATABASE'].presence || 'leihs') \
+        + '?pool=5'
+    else
+      'postgresql://localhost:5432/leihs_test?pool=5'
+    end
+  )
 end
 
 def clean_db
