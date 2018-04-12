09cc7f51901775493d81f92a0eeeab272355fc41
Matúš Kmiť <matus.kmit@zhdk.ch>
2018-04-12 10:45:52 +0200

    fix graphql url

--- spec/graphql/graphql_helper.rb
+++ spec/graphql/graphql_helper.rb
@@ -1,8 +1,11 @@
 require 'graphlient'
 
 RSpec.shared_context 'graphql client' do
+  let(:graphql_url) do
+    "#{ENV['LEIHS_HTTP_BASE_URL']}/procure/graphql"
+  end
   let(:graphql_client) do
-    Graphlient::Client.new(ENV['LEIHS_HTTP_BASE_URL'])
+    Graphlient::Client.new(graphql_url)
   end
 end
 
