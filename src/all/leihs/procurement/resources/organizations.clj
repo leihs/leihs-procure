e82e5cef52c5764b7211e413b45af4f3e65e4456
Matúš Kmiť <matus.kmit@zhdk.ch>
2018-04-16 16:49:37 +0200

    organizations hierarchy

--- src/all/leihs/procurement/resources/organizations.clj
+++ src/all/leihs/procurement/resources/organizations.clj
@@ -1,11 +1,36 @@
 (ns leihs.procurement.resources.organizations
-  (:require [clojure.java.jdbc :as jdbc]
-            [leihs.procurement.utils.sql :as sql]))
+  (:require [clj-logging-config.log4j :as logging-config]
+            [clojure.java.jdbc :as jdbc]
+            [clojure.tools.logging :as logging]
+            [leihs.procurement.utils.sql :as sql]
+            [logbug.debug :as debug]
+            ))
 
 (def organizations-base-query
   (-> (sql/select :procurement_organizations.*)
       (sql/from :procurement_organizations)))
 
-(defn get-organizations [context _ _]
+(defn organizations-query [_ args value]
+  (let [root-only (:root_only args)
+        id (:id value)]
+    (sql/format
+      (cond-> organizations-base-query
+        root-only
+        (sql/merge-where [:=
+                          :procurement_organizations.parent_id
+                          nil])
+        id
+        (sql/merge-where [:=
+                          :procurement_organizations.parent_id
+                          id])))))
+
+(defn get-organizations [context args value]
   (jdbc/query (-> context :request :tx)
-              (sql/format organizations-base-query)))
+              (organizations-query context args value)))
+
+;#### debug ###################################################################
+; (logging-config/set-logger! :level :debug)
+; (logging-config/set-logger! :level :info)
+; (debug/debug-ns 'cider-ci.utils.shutdown)
+; (debug/debug-ns *ns*)
+; (debug/undebug-ns *ns*)
