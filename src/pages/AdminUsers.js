b084788382155594f69945679614232aca2e4e6a
Max Albrecht <max.albrecht@zhdk.ch>
2018-04-20 17:29:33 +0200

    mutation with cache update

--- src/pages/AdminUsers.js
+++ src/pages/AdminUsers.js
@@ -1,4 +1,4 @@
-import React, { Fragement as F } from 'react'
+import React from 'react'
 import f from 'lodash'
 
 import { Query, Mutation } from 'react-apollo'
@@ -8,39 +8,49 @@ import ControlledForm from '../components/ControlledForm'
 import Icon from '../components/Icons'
 import { Row, Col } from '../components/Bootstrap'
 
+const mutationErrorHandler = err => {
+  // not much we can do on backend error
+  alert('Error! ' + err)
+  window.location.reload()
+}
+
 const UPDATE_ADMINS_MUTATION = gql`
   mutation updateAdmins($adminUserList: [AdminInput]) {
     admins(input_data: $adminUserList) {
       id
       firstname
       lastname
+      blubs
     }
   }
 `
 
-const UpdateAdmins = ({ admins }) => (
-  <Mutation mutation={UPDATE_ADMINS_MUTATION}>
-    {(updateAdmins, res) => (
-      <div>
-        <button
-          type="submit"
-          className="btn m-1 btn-primary"
-          onClick={e => {
-            e.preventDefault()
-            updateAdmins({
-              variables: { adminUserList: admins.map(id => ({ user_id: id })) }
-            })
-          }}>
-          <Icon.Checkmark /> <span>Speichern</span>
-        </button>
-        <h4>results:</h4>
-        <pre>{JSON.stringify(res, 0, 2)}</pre>
-      </div>
-    )}
-  </Mutation>
-)
+const USERS_QUERY = gql`
+  query AdminUsersPage {
+    admins {
+      id
+      firstname
+      lastname
+    }
+    # requesters_organizations {
+    #   user {
+    #     id
+    #     firstname
+    #     lastname
+    #   }
+    #   organization {
+    #     id
+    #     name
+    #   }
+    #   department {
+    #     id
+    #     name
+    #   }
+    # }
+  }
+`
 
-const AdminUsers = ({ data: { admins } }) => (
+const AdminUsers = ({ data: { admins }, updateData, updatingInfo }) => (
   <div className="pt-2 pb-3">
     <Row>
       <Col lg="2" />
@@ -63,8 +73,10 @@ const AdminUsers = ({ data: { admins } }) => (
                 className="XXXwas-validated"
                 onSubmit={e => {
                   e.preventDefault()
+                  updateData(fields)
                   // alert(JSON.stringify(fields, 0, 2))
-                }}>
+                }}
+              >
                 {/* <FormField label={'Administratoren'}> */}
                 <Row>
                   <Col sm="3">
@@ -94,10 +106,16 @@ const AdminUsers = ({ data: { admins } }) => (
                     })}
                   </Col>
                 </Row>
-                <UpdateAdmins admins={fields.admins} />
-                {/* <button type="submit" className="btn m-1 btn-primary">
+
+                <button
+                  type="submit"
+                  className="btn m-1 btn-primary"
+                  disabled={updatingInfo.loading}
+                >
                   <Icon.Checkmark /> <span>Speichern</span>
-                </button> */}
+                </button>
+
+                {/* <pre>{JSON.stringify(updatingInfo, 0, 2)}</pre> */}
               </form>
             )
           }}
@@ -118,40 +136,47 @@ const AdminUsers = ({ data: { admins } }) => (
   </div>
 )
 
-const USERS_QUERY = gql`
-  query AdminUsersPage {
-    admins {
-      id
-      firstname
-      lastname
-    }
-    # requesters_organizations {
-    #   user {
-    #     id
-    #     firstname
-    #     lastname
-    #   }
-    #   organization {
-    #     id
-    #     name
-    #   }
-    #   department {
-    #     id
-    #     name
-    #   }
-    # }
-  }
-`
-
 const AdminUsersPage = () => (
   <Query query={USERS_QUERY}>
     {({ loading, error, data }) => {
-      return error ? (
-        'Error! ' + error
-      ) : loading ? (
-        'Loading…'
-      ) : (
-        <AdminUsers data={data} />
+      if (loading) return <p>Loading...</p>
+      if (error)
+        return (
+          <p>
+            Error :( <code>{error}</code>
+          </p>
+        )
+
+      return (
+        <Mutation
+          mutation={UPDATE_ADMINS_MUTATION}
+          onError={mutationErrorHandler}
+          update={(cache, { data: { admins } }) => {
+            // update the internal cache with the new data we received:
+            cache.writeQuery({
+              query: USERS_QUERY,
+              data: { admins }
+            })
+          }}
+        >
+          {(updateAdmins, updatingInfo) => (
+            <div>
+              <AdminUsers
+                data={data}
+                updatingInfo={updatingInfo}
+                updateData={formData =>
+                  updateAdmins({
+                    variables: {
+                      adminUserList: formData.admins.map(id => ({
+                        user_id: id
+                      }))
+                    }
+                  })
+                }
+              />
+            </div>
+          )}
+        </Mutation>
       )
     }}
   </Query>
