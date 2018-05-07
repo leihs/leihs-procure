7d25abf3f1d3cfe4490fc6b86f8f5724c84e3c91
Matúš Kmiť <matus.kmit@zhdk.ch>
2018-05-07 12:18:41 +0200

    main category model spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,9 @@ require 'config/database'
 require 'config/factories'
 require 'pry'
 
+# Require factories as they contain model class definitions.
+# This is needed for models/* tests.
+# Dir["factories/*.rb"].each { |file| require file }
+
 RSpec.configure do |config|
 end
