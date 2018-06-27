fd6f517f126a87184cfff8426ef29b2112e96870
Max Albrecht <max.albrecht@zhdk.ch>
2018-06-27 15:10:28 +0200

    apollo: support anti-csrf

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -1,24 +1,33 @@
-// import f from 'lodash'
-import { ApolloClient } from 'apollo-boost'
-import { InMemoryCache } from 'apollo-boost'
-import { HttpLink } from 'apollo-boost'
+import ApolloClient from 'apollo-boost'
 
-// // TMP DEV FAKE AUTH
-// const USER_IDS = {
-//   mfa: '7da6733c-c819-5613-8cad-2a40f51c90da',
-//   gasser: 'f721d6b7-8275-5ee0-b225-aa7c13781f45'
-// }
-//
-// window.CURRENT_USER_ID = USER_IDS.gasser
+const CSRF_COOKIE_NAME = 'leihs-anti-csrf-token'
 
-// for dynamic header see <https://www.apollographql.com/docs/react/recipes/authentication.html#Header>
+// TODO: reenable dev fake auth
+const isDev = false
+// const isDev = process.env.NODE_ENV !== 'production'
+
+// DEV FAKE AUTH
+const USER_IDS = {
+  mfa: '7da6733c-c819-5613-8cad-2a40f51c90da',
+  gasser: 'f721d6b7-8275-5ee0-b225-aa7c13781f45'
+}
+
+window.LEIHS_DEV_CURRENT_USER_ID = USER_IDS.gasser
+
+const buildAuthHeaders = () =>
+  isDev
+    ? { FakeAuthorization: window.LEIHS_DEV_CURRENT_USER_ID }
+    : { 'x-csrf-token': getCSRFToken(document.cookie, CSRF_COOKIE_NAME) }
+
 export const apolloClient = new ApolloClient({
-  link: new HttpLink({
-    uri: '/procure/graphql',
-    // headers: {
-    //   authorization: window.CURRENT_USER_ID
-    // }
-    credentials: 'same-origin'
-  }),
-  cache: new InMemoryCache()
+  uri: '/procure/graphql',
+  credentials: isDev ? 'omit' : 'same-origin', // send the cookie(s)
+  request: operation => operation.setContext({ headers: buildAuthHeaders() })
 })
+
+const getCSRFToken = (cookies, name) =>
+  cookies
+    .split(';')
+    .map(s => s.trim())
+    .filter(s => s.indexOf(name) === 0)[0]
+    .replace(`${name}=`, '')
