242d9a7a5a0a86427f0a7b2d2334c5e2b1d41af4
Max Albrecht <max.albrecht@zhdk.ch>
2018-06-28 12:01:34 +0200

    apollo: fake auth in dev

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -1,27 +1,37 @@
 import ApolloClient from 'apollo-boost'
+import logger from 'debug'
+const log = logger('app:apollo')
 
 const CSRF_COOKIE_NAME = 'leihs-anti-csrf-token'
 
-// TODO: reenable dev fake auth
-const isDev = false
-// const isDev = process.env.NODE_ENV !== 'production'
+const isDev = process.env.NODE_ENV !== 'production'
 
 // DEV FAKE AUTH
-const USER_IDS = {
-  mfa: '7da6733c-c819-5613-8cad-2a40f51c90da',
-  gasser: 'f721d6b7-8275-5ee0-b225-aa7c13781f45'
-}
+isDev &&
+  (() => {
+    const USER_IDS = {
+      mfa: '7da6733c-c819-5613-8cad-2a40f51c90da',
+      gasser: 'f721d6b7-8275-5ee0-b225-aa7c13781f45'
+    }
 
-window.LEIHS_DEV_CURRENT_USER_ID = USER_IDS.gasser
+    window.LEIHS_DEV_CURRENT_USER_ID = USER_IDS.gasser
+
+    log('RUNNING IN DEV MODE', { fakeUser: window.LEIHS_DEV_CURRENT_USER_ID })
+  })()
 
 const buildAuthHeaders = () =>
   isDev
-    ? { FakeAuthorization: window.LEIHS_DEV_CURRENT_USER_ID }
-    : { 'x-csrf-token': getCSRFToken(document.cookie, CSRF_COOKIE_NAME) }
+    ? { 'X-Fake-Authorization': window.LEIHS_DEV_CURRENT_USER_ID }
+    : { 'X-CSRF-Token': getCSRFToken(document.cookie, CSRF_COOKIE_NAME) }
 
 export const apolloClient = new ApolloClient({
   uri: '/procure/graphql',
+  // static options for fetch requests:
   credentials: isDev ? 'omit' : 'same-origin', // send the cookie(s)
+  fetchOptions: {
+    credentials: isDev ? 'omit' : 'same-origin' // send the cookie(s)
+  },
+  // dynamic options for fetch requests:
   request: operation => operation.setContext({ headers: buildAuthHeaders() })
 })
 
@@ -29,4 +39,5 @@ const getCSRFToken = (cookies, name) =>
   (cookies || '')
     .split(';')
     .map(s => s.trim())
+    .filter(s => s && s.indexOf(name) === 0)[0]
     .replace(`${name}=`, '')
