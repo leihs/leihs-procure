bd89aa212d97dc030c31d9d47acd1088fed0e9ee
Max Albrecht <max.albrecht@zhdk.ch>
2018-06-29 16:05:48 +0200

    specs: Bootstrap.Button

--- client/src/components/Bootstrap/Bootstrap.examples.js
+++ client/src/components/Bootstrap/Bootstrap.examples.js
@@ -12,8 +12,25 @@ export const examples = [
         <Button>Button</Button>
         {_space_}
         <Button color="primary" title="test title">
-          Button
+          Button with title
         </Button>
+        {_space_}
+        <Button color="primary">Button primary</Button>
+        {_space_}
+        <Button color="secondary">Button secondary</Button>
+        {_space_}
+        <Button color="success">Button success</Button>
+        {_space_}
+        <Button color="danger">Button danger</Button>
+        {_space_}
+        <Button color="warning">Button warning</Button>
+        {_space_}
+        <Button color="info">Button info</Button>
+        {_space_}
+        <Button color="light">Button light</Button>
+        {_space_}
+        <Button color="dark">Button dark</Button>
+        {_space_}
         {_space_}
         <Button onClick={e => window.alert('clicked!')}>Button</Button>
         {_space_}
