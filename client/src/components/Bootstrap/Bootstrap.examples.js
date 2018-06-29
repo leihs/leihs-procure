61cad71ac411d010610005ae89842b2cc8cc7f18
Max Albrecht <max.albrecht@zhdk.ch>
2018-06-29 11:26:11 +0200

    spec/examples: Bootstrap.Badge

--- client/src/components/Bootstrap/Bootstrap.examples.js
+++ client/src/components/Bootstrap/Bootstrap.examples.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Button } from './Bootstrap'
+import { Button, Badge } from './Bootstrap'
 
 const _space_ = ' '
 
@@ -20,6 +20,33 @@ export const examples = [
         <Button color="link" title="test title" className="my-class">
           Button
         </Button>
+      </React.Fragment>
+    )
+  },
+  {
+    name: 'Badge',
+    content: (
+      <React.Fragment>
+        <h3>
+          Heading <Badge>Badge</Badge>
+        </h3>
+        <Badge>Badge</Badge>
+        {_space_}
+        <Badge primary>Badge primary</Badge>
+        {_space_}
+        <Badge secondary>Badge secondary</Badge>
+        {_space_}
+        <Badge success>Badge success</Badge>
+        {_space_}
+        <Badge danger>Badge danger</Badge>
+        {_space_}
+        <Badge warning>Badge warning</Badge>
+        {_space_}
+        <Badge info>Badge info</Badge>
+        {_space_}
+        <Badge light>Badge light</Badge>
+        {_space_}
+        <Badge dark>Badge dark</Badge>
       </React.Fragment>
     )
   }
