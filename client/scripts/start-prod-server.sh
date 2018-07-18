5db7b008e162656ccc4ea3f8588bd732b501b099
Max Albrecht <max.albrecht@zhdk.ch>
2018-07-18 09:13:19 +0200

    client: fix local prod start script
    - rename
    - serve as SPA
    - remove "homepage" from package.json, is now configured in deploy task
    - add 'start-dev-server' for consistency

--- client/scripts/start-prod-server.sh
+++ client/scripts/start-prod-server.sh
@@ -2,7 +2,7 @@
 
 { npm i && clear ;}
 { npm run build && clear ;}
-echo $(npx serve -p 5000 ./build && clear) &
+echo $(npx serve -s -p 5000 ./build && clear) &
 
 echo 'starting local proxy'
 npx micro-proxy -p 9000 -r proxy-dev.json
