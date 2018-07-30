50ab9c84a8b8f4884328c95e51fc539d54a2c2ee
Matus Kmit <matus.kmit@zhdk.ch>
2018-07-30 16:25:31 +0200

    upload main category image

--- server/spec/factories/upload.rb
+++ server/spec/factories/upload.rb
@@ -2,15 +2,22 @@ class Upload < Sequel::Model(:procurement_uploads)
 end
 
 FactoryBot.define do
-  f_name = 'secd.pdf'
-  f_path = "spec/files/#{f_name}"
-  me = MetadataExtractor.new(f_path)
-
   factory :upload, class: Upload do
-    filename f_name
+    transient do
+      real_filename 'secd.pdf'
+    end
+
+    filename { real_filename }
     content_type 'application/pdf'
     size 56000
-    content Base64.encode64(File.new(f_path).read)
-    metadata me.data.to_display_hash.to_json
+
+    after(:build) do |upload, evaluator|
+      file_path = "spec/files/#{evaluator.real_filename}"
+      md_ext = MetadataExtractor.new(file_path)
+      file = File.new(file_path)
+
+      upload.content = Base64.encode64(file.read)
+      upload.metadata = md_ext.data.to_display_hash.to_json
+    end
   end
 end
