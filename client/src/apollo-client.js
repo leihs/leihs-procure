aa4537cb1eb96f14163b8ccc066e69aa7822299a
Max Albrecht <max.albrecht@zhdk.ch>
2018-08-14 14:37:26 +0200

    client: dev: fake auth can be configured from browser

--- client/src/apollo-client.js
+++ client/src/apollo-client.js
@@ -4,28 +4,18 @@ import { InMemoryCache, defaultDataIdFromObject } from 'apollo-cache-inmemory'
 import logger from 'debug'
 const log = logger('app:apollo')
 
+const isDev =
+  process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test'
+const store = window.sessionStorage
+
 const CSRF_COOKIE_NAME = 'leihs-anti-csrf-token'
-
-const isDev = process.env.NODE_ENV !== 'production'
-
-// DEV FAKE AUTH
-isDev &&
-  (() => {
-    const USER_IDS = {
-      mfa: '7da6733c-c819-5613-8cad-2a40f51c90da',
-      gasser: 'f721d6b7-8275-5ee0-b225-aa7c13781f45'
-    }
-
-    window.LEIHS_DEV_CURRENT_USER_ID = USER_IDS.mfa
-
-    log('RUNNING IN DEV MODE', { fakeUser: window.LEIHS_DEV_CURRENT_USER_ID })
-  })()
-
 export const endpointURL = '/procure/graphql'
 
 export const buildAuthHeaders = () =>
   isDev
-    ? { 'X-Fake-Token-Authorization': window.LEIHS_DEV_CURRENT_USER_ID }
+    ? {
+        'X-Fake-Token-Authorization': store.getItem('LEIHS_DEV_FAKE_USER_ID')
+      }
     : { 'X-CSRF-Token': getCookieValue(document.cookie, CSRF_COOKIE_NAME) }
 
 export const fetchOptions = {
@@ -64,3 +54,22 @@ const getCookieValue = (cookies, name) =>
       .map(s => s.trim())
       .filter(s => s && s.indexOf(name) === 0)[0] || ''
   ).replace(`${name}=`, '')
+
+// DEV FAKE AUTH
+isDev &&
+  (() => {
+    const USER_IDS = {
+      mfa: '7da6733c-c819-5613-8cad-2a40f51c90da',
+      gasser: 'f721d6b7-8275-5ee0-b225-aa7c13781f45'
+    }
+    const fakeLogin = nameOrId => {
+      store.setItem('LEIHS_DEV_FAKE_USER_ID', USER_IDS[nameOrId] || nameOrId)
+      window.location.reload()
+    }
+    window.LEIHS_DEV_USER_IDS = USER_IDS
+    window.fakeLogin = fakeLogin
+
+    const fakeUser = store.getItem('LEIHS_DEV_FAKE_USER_ID')
+    if (!fakeUser) fakeLogin('mfa')
+    log('RUNNING IN DEV MODE', { fakeUser })
+  })()
