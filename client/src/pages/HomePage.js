6834386e87af553958478b3c6ad9db416985d723
Max Albrecht <max.albrecht@zhdk.ch>
2018-08-16 15:17:18 +0200

    ui: redirect from root to /requests dashboard

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,22 +1,7 @@
 import React from 'react'
 // import f from 'lodash'
-import { NavLink } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 
-import { ListGroup, ListGroupItem } from '../components/Bootstrap'
-import { MainWithSidebar } from '../components/Layout'
-
-const ListGroupLink = p => <ListGroupItem {...p} tag={NavLink} />
-
-const Homepage = p => (
-  <MainWithSidebar>
-    <ListGroup className="mt-5">
-      <ListGroupLink to="/requests/new">Neuen Antrag erstellen</ListGroupLink>
-      {/* TODO: link to 'my requests' (needs param handling on RequestsIndex) 
-        <ListGroupLink to='/requests'>Meine Anträge ansehen</ListGroupLink>
-      */}
-      <ListGroupLink to="/requests">Alle Anträge ansehen</ListGroupLink>
-    </ListGroup>
-  </MainWithSidebar>
-)
+const Homepage = p => <Redirect to="/requests" />
 
 export default Homepage
