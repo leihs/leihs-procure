a5b1416bd136b9aa217466288c3dadbf172503cd
Matus Kmit <matus.kmit@zhdk.ch>
2018-08-25 16:37:12 +0200

    sort users

--- server/src/all/leihs/procurement/resources/users.clj
+++ server/src/all/leihs/procurement/resources/users.clj
@@ -21,27 +21,32 @@
           exclude-ids (:exclude_ids args)
           offset (:offset args)
           limit (:limit args)]
-      (sql/format
-        (cond-> users-base-query
-          is-requester (sql/merge-join
-                         :procurement_requesters_organizations
-                         [:= :procurement_requesters_organizations.user_id
-                          :users.id])
-          term-parts (sql/merge-where
-                       (into [:and]
-                             (map (fn [term-percent]
-                                    ["~~*"
-                                     (->>
-                                       (sql/call :concat
-                                                 :users.firstname
-                                                 (sql/call :cast " " :varchar)
-                                                 :users.lastname)
-                                       (sql/call :unaccent))
-                                     (sql/call :unaccent term-percent)])
-                               term-parts)))
-          exclude-ids (sql/merge-where [:not-in :users.id exclude-ids])
-          offset (sql/offset offset)
-          limit (sql/limit limit))))))
+      (-> (cond-> users-base-query is-requester
+                  (sql/merge-join :procurement_requesters_organizations
+                                  [:=
+                                   :procurement_requesters_organizations.user_id
+                                   :users.id])
+                    term-parts
+                  (sql/merge-where
+                    (into [:and]
+                          (map
+                            (fn [term-percent]
+                              ["~~*"
+                               (->> (sql/call :concat
+                                              :users.firstname
+                                              (sql/call :cast " " :varchar)
+                                              :users.lastname)
+                                    (sql/call :unaccent))
+                               (sql/call :unaccent term-percent)])
+                            term-parts)))
+                    exclude-ids
+                  (sql/merge-where [:not-in :users.id exclude-ids]) offset
+                  (sql/offset offset) limit
+                  (sql/limit limit))
+          (sql/order-by (sql/call :concat
+                                  :users.firstname :users.lastname
+                                  :users.login :users.id))
+          sql/format))))
 
 ;#### debug ###################################################################
 ; (logging-config/set-logger! :level :debug)
