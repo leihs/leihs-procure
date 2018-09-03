dbeecfa9cfc3441785478cdd8becc77e4327cc7f
Max Albrecht <max.albrecht@zhdk.ch>
2018-09-03 13:33:43 +0200

    ui: router: slightly wait before scrolling to top

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -3,7 +3,7 @@ import { Redirect as RRRedirect } from 'react-router-dom'
 
 export const Redirect = ({ scrollTop = true, ...props }) => {
   if (scrollTop && window) {
-    window.scrollTo(0, 0)
+    setTimeout(() => window.scrollTo(0, 0), 1)
   }
 
   return <RRRedirect {...props} />
