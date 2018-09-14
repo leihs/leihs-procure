leihs/leihs-procure

commit 17abc2c38b63f153c7e79f983d16389c4b97292a

ui: decorators: RequestFieldValue

client/src/components/RequestForm.js
@@ -23,7 +23,11 @@ import {
   DropdownItem
 } from './Bootstrap'
 
-import { RequestTotalAmount as TotalAmount, formatCurrency } from './decorators'
+import {
+  RequestTotalAmount as TotalAmount,
+  RequestFieldValue as FieldValue,
+  formatCurrency
+} from './decorators'
 import * as formBlocker from './FormBlocker'
 import { ConfirmFormNav } from './FormBlocker'
 import BuildingAutocomplete from './BuildingAutocomplete'
@@ -308,13 +312,7 @@ class RequestForm extends React.Component {
                       <FormGroup horizontal={compactView} label={field.label}>
                         <ButtonRadio
                           {...formPropsFor('replacement')}
-                          value={
-                            f.isBoolean(field.value)
-                              ? CONSTANTS.REQUEST_REPLACEMENT_VALUES_MAP[
-                                  field.value
-                                ]
-                              : field.value
-                          }
+                          value={FieldValue('replacement', request)}
                           options={CONSTANTS.REQUEST_REPLACEMENT_VALUES.map(
                             v => ({
                               value: v,

client/src/components/decorators.js
@@ -3,6 +3,9 @@ import f from 'lodash'
 import { DateTime } from 'luxon'
 import { formatMoney } from 'accounting-js'
 
+import * as CONSTANTS from '../constants'
+import t from '../locale/translate'
+
 const isDev = process.env.NODE_ENV !== 'production'
 
 export const DisplayName = (o, { short = false, abbr = false } = {}) => {
@@ -73,6 +76,30 @@ export const budgetPeriodDates = bp => {
   const isRequesting = !isPast && now <= inspectStartDate
   const isInspecting = !isPast && !isRequesting
   return { inspectStartDate, endDate, isPast, isRequesting, isInspecting }
+}
+
+export const RequestFieldValue = (name, request) => {
+  const r = request
+  switch (name) {
+    case 'state':
+      return t(`request_state_label_${r.state}`)
+    case 'replacement':
+      return t(
+        `request_replacement_labels_${
+          CONSTANTS.REQUEST_REPLACEMENT_VALUES_MAP[r.replacement.value]
+        }`
+      )
+    case 'priority':
+      return t(`priority_label_${r.priority.value}`)
+    case 'inspector_priority':
+      return t(`inspector_priority_label_${r.inspector_priority.value}`)
+    case 'accounting_type':
+      return t(
+        `request_form_field.accounting_type_label_${r.accounting_type.value}`
+      )
+    default:
+      throw new Error(`Unknown RequestField name '${name}'`)
+  }
 }
 
 export const RequestTotalAmount = fields => {

commit 7fb6d002731c5e7d01d42fac6d7c4642fae4bbd1

fix: RequestForm: replacement radio
Was broken in 17abc2c38b63f153c7e79f983d16389c4b97292a

decorator is for stringified value, should not have been used in form selection

client/src/components/RequestForm.js
@@ -23,11 +23,7 @@ import {
   DropdownItem
 } from './Bootstrap'
 
-import {
-  RequestTotalAmount as TotalAmount,
-  RequestFieldValue as FieldValue,
-  formatCurrency
-} from './decorators'
+import { RequestTotalAmount as TotalAmount, formatCurrency } from './decorators'
 import * as formBlocker from './FormBlocker'
 import { ConfirmFormNav } from './FormBlocker'
 import BuildingAutocomplete from './BuildingAutocomplete'
@@ -312,7 +308,13 @@ class RequestForm extends React.Component {
                       <FormGroup horizontal={compactView} label={field.label}>
                         <ButtonRadio
                           {...formPropsFor('replacement')}
-                          value={FieldValue('replacement', request)}
+                          value={
+                            f.isBoolean(field.value)
+                              ? CONSTANTS.REQUEST_REPLACEMENT_VALUES_MAP[
+                                  field.value
+                                ]
+                              : field.value
+                          }
                           options={CONSTANTS.REQUEST_REPLACEMENT_VALUES.map(
                             v => ({
                               value: v,