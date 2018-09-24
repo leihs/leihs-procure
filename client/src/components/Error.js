703cd3c0bc74b8d059754eb0047b226e29b45b8d
Max Albrecht <max.albrecht@zhdk.ch>
2018-09-24 14:53:58 +0200

    ui: translate errors

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -2,49 +2,53 @@ import React from 'react'
 import f from 'lodash'
 import { DateTime } from 'luxon'
 import BrowserInfo from 'browser-info'
+import t from '../locale/translate'
 
 const browserLine = BrowserLine()
 
-export const FatalErrorScreen = ({ error, children, title = 'ERROR' }) => (
-  <div
-    style={{
-      background: 'blue',
-      padding: '1rem',
-      height: '100vh',
-      width: '100vw',
-      boxSizing: 'border-box',
-      display: 'table',
-      tableLayout: 'fixed'
-    }}
-  >
+export const FatalErrorScreen = ({ error, children, title }) => {
+  title = f.presence(title) || t('errors.error_title')
+  return (
     <div
       style={{
-        background: 'white',
-        textAlign: 'center',
-        padding: '2rem',
-        display: 'table-cell',
-        verticalAlign: 'middle'
+        background: 'blue',
+        padding: '1rem',
+        height: '100vh',
+        width: '100vw',
+        boxSizing: 'border-box',
+        display: 'table',
+        tableLayout: 'fixed'
       }}
     >
-      {!!title && <h1 className="h4">{title}</h1>}
-      <div>{!!children && children}</div>
-      {!!error && (
-        <p>
-          <JsonDetails
-            error={{
-              error,
-              env: {
-                target: process.env.NODE_ENV,
-                browser: browserLine,
-                time: (d => [new DateTime(d).toISO(), d.toJSON()])(new Date())
-              }
-            }}
-          />
-        </p>
-      )}
+      <div
+        style={{
+          background: 'white',
+          textAlign: 'center',
+          padding: '2rem',
+          display: 'table-cell',
+          verticalAlign: 'middle'
+        }}
+      >
+        {!!title && <h1 className="h4">{title}</h1>}
+        <div>{!!children && children}</div>
+        {!!error && (
+          <p>
+            <JsonDetails
+              error={{
+                error,
+                env: {
+                  target: process.env.NODE_ENV,
+                  browser: browserLine,
+                  time: (d => [new DateTime(d).toISO(), d.toJSON()])(new Date())
+                }
+              }}
+            />
+          </p>
+        )}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export const ErrorPanel = ({ error, data, errorDetails }) => {
   const apiErrors = f.presence(f.get(error, 'graphQLErrors'))
@@ -64,18 +68,20 @@ export const ErrorPanel = ({ error, data, errorDetails }) => {
       <pre>{error.toString()}</pre>
 
       {errorDetails && (
-        <JsonDetails title="Error Details:" error={errorDetails} />
+        <JsonDetails title={t('errors.error_details')} error={errorDetails} />
       )}
 
       {networkError && (
-        <JsonDetails title="Network Error:" error={networkError} />
+        <JsonDetails title={t('errors.network_error')} error={networkError} />
       )}
 
       {apiErrors && (
-        <JsonDetails title="API Error Details:" error={apiErrors} />
+        <JsonDetails title={t('errors.api_error_details')} error={apiErrors} />
       )}
 
-      {!f.isEmpty(data) && <JsonDetails title="Data:" error={data} />}
+      {!f.isEmpty(data) && (
+        <JsonDetails title={t('errors.data_details')} error={data} />
+      )}
 
       <p className="text-muted mb-0">
         <small>
@@ -83,7 +89,7 @@ export const ErrorPanel = ({ error, data, errorDetails }) => {
             {new Date().toJSON()}
             {'  '}
           </samp>
-          <span>¯\_༼ ༎ຶ ෴ ༎ຶ༽_/¯</span>
+          <span>{t('errors.error_panel_emoticon')}</span>
           <samp>
             {'  '}
             {browserLine}
@@ -107,7 +113,7 @@ export function getErrorCode(error) {
   }
 }
 
-const JsonDetails = ({ error, title = 'Show details' }) =>
+const JsonDetails = ({ error, title = t('errors.show_details') }) =>
   !!error && (
     <details>
       <summary>
