e7a36c92f864ff80a0d08603e7c77b7ef04b3ea6
Max Albrecht <max.albrecht@zhdk.ch>
2019-01-09 09:26:09 +0100

    add pseudo-languages for localization dev & test

--- client/src/locale/translate.js
+++ client/src/locale/translate.js
@@ -1,8 +1,28 @@
 import f from 'lodash'
+import { isDev } from '../env'
 
-import translations from './de.json'
+// real langs
+import en from './en.json'
+import de from './de.json'
+
+// dev langs
+import xx from './xx.json'
+import zz from './zz.json'
+// dev lang stuff
+const DEV_LANGS = isDev ? { xx, zz } : null
+const store = window.sessionStorage
+const SELECTED_LANG = store.getItem('LEIHS_DEV_FAKE_USER_LANG')
+const fakeLang = l => {
+  store.setItem('LEIHS_DEV_FAKE_USER_LANG', l)
+  window.location.reload()
+}
+window && (window.fakeLang = fakeLang)
+
+const LANGS = { en, de, ...DEV_LANGS }
+const DEFAULT_LANG = 'de'
 
 const translate = key => {
+  const translations = LANGS[SELECTED_LANG] || LANGS[DEFAULT_LANG]
   const fallback = `⟪${key}⟫`
   // 'foo.bar.baz' => [ 'foo.bar.baz', 'bar.baz', 'baz' ]
   const paths = key.split('.').map((i, n, a) => a.slice(n).join('.'))
