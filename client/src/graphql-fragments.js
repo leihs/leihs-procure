leihs/leihs-procure

commit 7207adc00b8be0db25ce4c38b8086f8ee1afdb2d

Sums for requester consider only requested_quantity for budget period not past (#52)
* spec wip

* fix total price request

* ports in scripts

* fix test

* primes

* remove unused var

* optimize and fix test

* extending spec

* spec complete

client/src/graphql-fragments.js
@@ -74,6 +74,7 @@ export const RequestFieldsForIndex = gql`
     price_cents {
       value
     }
+    total_price_cents 
     price_currency {
       value
     }

client/.graphqlconfig
@@ -4,7 +4,7 @@
       "schemaPath": "schema.graphql",
       "extensions": {
         "endpoints": {
-          "default": "http://localhost:3003/procure/graphql"
+          "default": "http://localhost:3230/procure/graphql"
         }
       }
     }

client/src/components/RequestLine.js
@@ -3,7 +3,7 @@ import cx from 'classnames'
 // import f from 'lodash'
 
 import t from '../locale/translate'
-import { DisplayName, RequestTotalAmount, formatCurrency } from './decorators'
+import { DisplayName, formatCurrency } from './decorators'
 import { Row, Col, Badge, Tooltipped } from './Bootstrap'
 import Icon from './Icons'
 import RequestStateBadge from './RequestStateBadge'
@@ -133,7 +133,7 @@ export const RequestLineClosed = ({ request, onClick, className }) => (
     <Col sm="2" className="align-self-center text-right">
       <Tooltipped text={t('request_form_field.price_total')}>
         <span className="mr-2 f6" id={`totalam_tt_${request.id}`}>
-          <samp>{formatCurrency(RequestTotalAmount(request))} </samp>
+          <samp>{formatCurrency(request.total_price_cents)} </samp>
           <Icon.ShoppingCart />
         </span>
       </Tooltipped>

server/scripts/start-dev-repl
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 export LEIHS_SECRET="secret"
-export LEIHS_PROCURE_HTTP_BASE_URL="http://localhost:3003"
-export LEIHS_DATABASE_URL="jdbc:postgresql://localhost:5432/leihs_procure2_test?max-pool-size=5"
+export LEIHS_PROCURE_HTTP_BASE_URL="http://localhost:3230"
+export LEIHS_DATABASE_URL="jdbc:postgresql://localhost:5432/leihs_prod?max-pool-size=5"
 
 # cd database
 # RAILS_ENV=test bundle exec rake db:environment:set || true # fuck that

server/scripts/start-dev-server
@@ -7,7 +7,7 @@
 # echo ' - - - 8< - - - '
 
 export LEIHS_SECRET="secret"
-export LEIHS_PROCURE_HTTP_BASE_URL="http://localhost:3003"
+export LEIHS_PROCURE_HTTP_BASE_URL="http://localhost:3230"
 export LEIHS_DATABASE_URL="jdbc:postgresql://localhost:5432/leihs_procure2?max-pool-size=5"
 
 test -f resources/dev/log4j.properties \

server/scripts/start-test-repl
@@ -1,9 +1,9 @@
 #!/bin/sh -eu
 # source ./scripts/.helpers.sh
 
-export DATABASE_URL="postgresql://localhost:5432/leihs_procure2_test?max-pool-size=5"
+export DATABASE_URL="postgresql://localhost:5432/leihs_test?max-pool-size=5"
 export LEIHS_DATABASE_URL="jdbc:${DATABASE_URL}"
-export LEIHS_PROCURE_HTTP_BASE_URL="http://localhost:3004"
+export LEIHS_PROCURE_HTTP_BASE_URL="http://localhost:3230"
 export LEIHS_SECRET=secret
 
 lein with-profile test repl

server/src/all/leihs/procurement/resources/request.clj
@@ -163,10 +163,16 @@
   (assoc row :attachments :unqueried))
 
 (defn add-total-price
-  [row]
-  (let [quantity (or (:order_quantity row)
-                     (:approved_quantity row)
-                     (:requested_quantity row))]
+  [row advanced-user?]
+  (let [transparent-quantity (or (:order_quantity row)
+                                 (:approved_quantity row)
+                                 (:requested_quantity row))
+        quantity
+          (if (-> row
+                  :budget_period
+                  :is_past)
+            transparent-quantity
+            (if advanced-user? transparent-quantity (:requested_quantity row)))]
     (->> row
          :price_cents
          (* quantity)
@@ -201,18 +207,21 @@
        (assoc row :procurement_account)))
 
 (defn transform-row
-  [row]
+  [row advanced-user?]
   (-> row
       enum-state
       add-general-ledger-account
       add-cost-center
       add-procurement-account
-      add-total-price
+      (add-total-price advanced-user?)
       treat-priority
       treat-inspector-priority
       initialize-attachments-attribute))
 
-(defn query-requests [tx query] (jdbc/query tx query {:row-fn transform-row}))
+(defn query-requests
+  [tx auth-entity query]
+  (let [advanced-user? (user-perms/advanced? tx auth-entity)]
+    (jdbc/query tx query {:row-fn #(transform-row % advanced-user?)})))
 
 (defn get-request-by-id-sqlmap
   [tx auth-entity id]
@@ -227,7 +236,7 @@
        (get-request-by-id-sqlmap tx auth-entity)
        request-helpers/join-and-nest-associated-resources
        sql/format
-       (query-requests tx)
+       (query-requests tx auth-entity)
        first))
 
 (defn- consider-default
@@ -272,7 +281,7 @@
         (sql/order-by [:created_at :desc])
         (sql/limit 1)
         sql/format
-        (->> (query-requests tx))
+        (->> (query-requests tx auth-entity))
         first)))
 
 (defn insert!
@@ -491,7 +500,7 @@
      (-> requests-base-query
          (sql/merge-where [:= :procurement_requests.id (:id request)])
          sql/format
-         (->> (query-requests tx))
+         (->> (query-requests tx auth-entity))
          first
          :user_id)))
 

commit 83cf6017f983e6c527447db940eb92c4ed68321a

fix: `newRequestQuery` should not ask for `total_price_cents`
regression from https://github.com/leihs/leihs-procure/commit/7207adc00b8be0db25ce4c38b8086f8ee1afdb2d

field is only needed on "index" view, moved there

client/src/graphql-fragments.js
@@ -74,7 +74,6 @@ export const RequestFieldsForIndex = gql`
     price_cents {
       value
     }
-    total_price_cents
     price_currency {
       value
     }

client/src/pages/RequestsIndexPage.js
@@ -94,6 +94,7 @@ const REQUESTS_QUERY = gql`
 
             requests {
               ...RequestFieldsForIndex
+              total_price_cents
               actionPermissions {
                 edit
               }