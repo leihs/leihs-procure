8eb501366d1dd192610d900a437819bc4c7cf1ee
Max Albrecht <max.albrecht@zhdk.ch>
2019-02-12 15:05:42 +0100

    lint

--- server/src/dev+test/leihs/procurement/auth/session.clj
+++ server/src/dev+test/leihs/procurement/auth/session.clj
@@ -1,13 +1,18 @@
 (ns leihs.procurement.auth.session
-  (:require [leihs.procurement.resources.user :as user]))
+  (:require
+    [leihs.procurement.resources.user :as user]))
 
 (defn wrap
   [handler]
   (fn [request]
-    (let [user-id (some-> request
-                          :headers
-                          (get "x-fake-token-authorization")
-                          (->> (user/get-user-by-id (:tx request)))
-                          :id)]
-      (handler (cond-> request
-                 user-id (assoc :authenticated-entity {:user_id user-id}))))))
+    (let [user-id
+            (some->
+              request
+              :headers
+              (get "x-fake-token-authorization")
+              (->>
+                (user/get-user-by-id (:tx request)))
+              :id)]
+      (handler
+        (cond-> request
+          user-id (assoc :authenticated-entity {:user_id user-id}))))))
