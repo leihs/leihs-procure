367c6a4d3c05a90e68452b904d71bb526484b088
Matus Kmit <matus.kmit@zhdk.ch>
2021-01-04 16:47:33 +0100

    namespace reload dev setup resurrected

---
+++ src/dev/app_setup.clj
@@ -0,0 +1,7 @@
+(ns app-setup
+  (:require [leihs.admin.main :as main]
+            [leihs.core.http-server :as http-server]))
+
+(defn stop [] (http-server/stop))
+
+(defn run [& args] (apply main/-main "run" args))
