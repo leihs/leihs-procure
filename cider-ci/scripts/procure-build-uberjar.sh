dc552fa471945decbf681494aab0bbea2eb1c07e
Max Albrecht <max.albrecht@zhdk.ch>
2021-03-26 13:59:04 +0100

    ci: build fix

--- cider-ci/scripts/procure-build-uberjar.sh
+++ cider-ci/scripts/procure-build-uberjar.sh
@@ -6,8 +6,11 @@ function build_uberjar() {
   echo "INFO: building the ${UBERJAR_NAME} uberjar now"
   cd $LEIHS_PROCURE_DIR/client
   export PUBLIC_URL="/procure"
-  npm ci || npm install # for building
-  npm run ci-build
+  # TMP workaround:
+  { cd leihs-ui/bootstrap-theme-leihs && npm install && cd - ;} || exit 1
+  { cd leihs-ui && npm install && cd - ;} || exit 1
+  npm install || exit 1
+  npm run ci-build || exit 1
   cd $LEIHS_PROCURE_DIR/server
   export LEIN_SNAPSHOTS_IN_RELEASE=yes
   lein uberjar
