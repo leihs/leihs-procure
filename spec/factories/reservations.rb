a53137102f268c4cf0fe2f7c05a7ce9acc20e479
Matus Kmit <matuskmit1@gmail.com>
2021-07-27 10:46:56 +0200

    fix reservations factory

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -6,6 +6,8 @@ FactoryBot.define do
     user_id { User.all.sample.id }
     inventory_pool_id { InventoryPool.all.sample.id }
     status { "approved" }
+    start_date { Date.tomorrow.to_s }
+    end_date { (Date.tomorrow + 1.day).to_s }
     created_at { Time.now }
     updated_at { Time.now }
   end
