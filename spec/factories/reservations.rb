c5077cf3b2309f19b83cb3e9141798740773d332
Matus Kmit <matuskmit1@gmail.com>
2021-11-16 14:58:29 +0100

    db: model_id or option_id constraint on reservations

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -1,10 +1,12 @@
 class Reservation < Sequel::Model(:reservations)
+  many_to_one(:leihs_model, key: :model_id)
 end
 
 FactoryBot.define do
   factory :reservation do
     user_id { User.all.sample.id }
     inventory_pool_id { InventoryPool.all.sample.id }
+    leihs_model
     status { "approved" }
     start_date { Date.tomorrow.to_s }
     end_date { (Date.tomorrow + 1.day).to_s }
