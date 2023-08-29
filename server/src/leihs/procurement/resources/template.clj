1e4b34fd9b58db59a978ce07b047137a4b1dc530
Matus Kmit <matuskmit1@gmail.com>
2023-08-29 16:58:36 +0200

    fix validation

--- server/src/leihs/procurement/resources/template.clj
+++ server/src/leihs/procurement/resources/template.clj
@@ -1,9 +1,9 @@
 (ns leihs.procurement.resources.template
   (:require [clojure.java.jdbc :as jdbc]
             [clojure.data :refer [diff]]
-            [clojure.test :as t]
             [leihs.core.core :refer [raise]]
-            [leihs.procurement.utils.sql :as sql]))
+            [leihs.procurement.utils.sql :as sql]
+            [taoensso.timbre :refer [debug info warn error]]))
 
 (def templates-base-query
   (-> (sql/select :procurement_templates.*)
@@ -24,49 +24,20 @@
                      (sql/values [tmpl])
                      sql/format)))
 
-(t/with-test
-  (defn valid-update-maps-for-used-template? [tmpl-before tmpl-after]
-    (let [[only-in-before only-in-after _] (diff tmpl-before tmpl-after)]
-      (or (and (empty? only-in-before) (empty? only-in-after))
-          (and (= (keys only-in-before) [:is_archived])
-               (= (keys only-in-after) [:is_archived])))))
-  (t/is (= (valid-update-maps-for-used-template? {:is_archived false}
-                                                 {:is_archived true})
-           true)
-        "True if only is_archived has been provided and has changed too.")
-  (t/is (= (valid-update-maps-for-used-template? {:is_archived true, :article_name "foo"}
-                                                 {:is_archived false, :article_name "foo"})
-           true)
-        "True if among others only is_archived attribute has changed.")
-  (t/is (= (valid-update-maps-for-used-template? {:is_archived true, :article_name "foo"}
-                                                 {:is_archived false, :article_name "bar"})
-           false)
-        "False if also some other attribute has changed.")
-  (t/is (= (valid-update-maps-for-used-template? {:is_archived true, :article_name "foo"}
-                                                 {:is_archived true, :article_name "foo"})
-           true)
-        "True if nothing has changed."))
-
-; (test #'valid-update-maps-for-used-template?)
-
-(defn validate-update-attributes! [tx tmpl-after]
-  (let [tmpl-before (get-template-by-id tx (:id tmpl-after))
-        req-exist? (-> (sql/select :%count.*)
+(defn validate-update-attributes [tx tmpl]
+  (let [req-exist? (-> (sql/select :%count.*)
                        (sql/from :procurement_requests)
-                       (sql/where [:= :template_id (:id tmpl-before)])
+                       (sql/where [:= :template_id (:id tmpl)])
                        sql/format
                        (->> (jdbc/query tx))
                        first :count (> 0))]
-    (when (and req-exist?
-               (not (valid-update-maps-for-used-template? tmpl-before tmpl-after)))
-      (raise "A used template can only be archived or unarchived."))))
+    (cond-> tmpl req-exist? (select-keys [:is_archived]))))
 
 (defn update-template!
   [tx tmpl]
-  (validate-update-attributes! tx tmpl)
   (jdbc/execute! tx
                  (-> (sql/update :procurement_templates)
-                     (sql/sset tmpl)
+                     (sql/sset (validate-update-attributes tx tmpl))
                      (sql/where [:= :procurement_templates.id (:id tmpl)])
                      sql/format)))
 
