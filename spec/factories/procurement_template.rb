411e8d20119d1372253363e6838d1b3acd99ebf1
Jan Pistor <pistor.jan@gmail.com>
2023-09-11 13:21:43 +0200

    refactor: add fields template factory

--- spec/factories/procurement_template.rb
+++ spec/factories/procurement_template.rb
@@ -1,15 +1,17 @@
-class ProcumentTemplate < Sequel::Model(:procurement_template)
+# frozen_string_literal: true
+
+class ProcurementTemplate < Sequel::Model(:procurement_templates)
   many_to_one :category, class: ProcurementCategory
 end
 
 FactoryBot.define do
   factory :procurement_template do
     article_name { Faker::Device.model_name }
-    article_number { Faker::Camera.model_name }
+    article_number { Faker::Device.model_name }
     price_cents { Faker::Commerce.price }
     price_currency { Faker::Currency.code }
     supplier_name { Faker::Camera.brand }
-    is_archived { Faker::Boolean.boolean }
+    is_archived { true }
     association :category, factory: :procurement_category
   end
 end
