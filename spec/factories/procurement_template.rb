339c6c216fc31ebf3fdd53935f7370a7b2b3bdae
Jan Pistor <pistor.jan@gmail.com>
2023-09-11 11:29:39 +0200

    test: add template factory

---
+++ spec/factories/procurement_template.rb
@@ -0,0 +1,15 @@
+class ProcumentTemplate < Sequel::Model(:procurement_template)
+  many_to_one :category, class: ProcurementCategory
+end
+
+FactoryBot.define do
+  factory :procurement_template do
+    article_name { Faker::Device.model_name }
+    article_number { Faker::Camera.model_name }
+    price_cents { Faker::Commerce.price }
+    price_currency { Faker::Currency.code }
+    supplier_name { Faker::Camera.brand }
+    is_archived { Faker::Boolean.boolean }
+    association :category, factory: :procurement_category
+  end
+end
