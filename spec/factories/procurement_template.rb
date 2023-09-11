214dd656f6da7e556b37b0361ce12925be0f6f16
Jan Pistor <pistor.jan@gmail.com>
2023-09-11 16:31:35 +0200

    test: added traits to template factory

--- spec/factories/procurement_template.rb
+++ spec/factories/procurement_template.rb
@@ -6,12 +6,32 @@ end
 
 FactoryBot.define do
   factory :procurement_template do
-    article_name { Faker::Device.model_name }
-    article_number { Faker::Device.model_name }
-    price_cents { Faker::Commerce.price }
-    price_currency { Faker::Currency.code }
-    supplier_name { Faker::Camera.brand }
-    is_archived { false }
-    association :category, factory: :procurement_category
+    trait :base do
+      article_name { Faker::Device.model_name }
+      article_number { Faker::Device.model_name }
+      price_cents { Faker::Commerce.price }
+      price_currency { Faker::Currency.code }
+      supplier_name { Faker::Camera.brand }
+    end
+
+    trait :deletable do
+      include :base
+      is_archived { false }
+      association :category, factory: :procurement_category
+    end
+
+    trait :archiveable do
+      include :base
+      is_archived { false }
+      association :category, factory: :procurement_category
+    end
+
+    trait :unarchiveable do
+      include :base
+      is_archived { true }
+      association :category, factory: :procurement_category
+    end
+
+    default_traits :deletable
   end
 end
