f53dc961312c2338b293518263275abc9843bd9d
Jan Pistor <pistor.jan@gmail.com>
2023-09-18 11:45:18 +0200

    chore(deps): update pachakges
    - update all packages to their latest minor
    
    - update all dev related packages to ther latest version

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
   server: {
     port: 4000,
     proxy: {
