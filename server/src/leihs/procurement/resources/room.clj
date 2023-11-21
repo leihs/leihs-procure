1ab817389c8df925269cfd5b9d3f15685eeef91b
mradl <manuel.radl@zhdk.ch>
2023-11-21 15:42:30 +0100

    change v25

--- server/src/leihs/procurement/resources/room.clj
+++ server/src/leihs/procurement/resources/room.clj
@@ -1,15 +1,22 @@
 (ns leihs.procurement.resources.room
-  (:require [clojure.java.jdbc :as jdbc]
-            [leihs.procurement.utils.sql :as sql]))
+  (:require 
+    ;[clojure.java.jdbc :as jdbc]
+    ;        [leihs.procurement.utils.sql :as sql]
+
+    [honey.sql :refer [format] :rename {format sql-format}]
+    [leihs.core.db :as db]
+    [next.jdbc :as jdbc]
+    [honey.sql.helpers :as sql]
+    ))
 
 (defn room-query
   [id]
   (-> (sql/select :rooms.*)
       (sql/from :rooms)
       (sql/where [:= :rooms.id id])
-      sql/format))
+      sql-format))
 
-(defn get-room-by-id [tx id] (first (jdbc/query tx (room-query id))))
+(defn get-room-by-id [tx id] ( (jdbc/execute-one! tx (room-query id))))
 
 (defn get-room
   [context _ value]
