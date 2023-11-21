2a8cb5fd79be2586b72831ae29fa28e5df8d9af0
mradl <manuel.radl@zhdk.ch>
2023-11-21 14:07:09 +0100

    change v6

--- server/src/leihs/procurement/resources/budget_limits.clj
+++ server/src/leihs/procurement/resources/budget_limits.clj
@@ -1,6 +1,13 @@
 (ns leihs.procurement.resources.budget-limits
-  (:require [clojure.java.jdbc :as jdbc]
-            [leihs.procurement.utils.sql :as sql]))
+  (:require
+    ;[clojure.java.jdbc :as jdbc]
+    ;        [leihs.procurement.utils.sql :as sql]
+
+    [honey.sql :refer [format] :rename {format sql-format}]
+    [leihs.core.db :as db]
+    [next.jdbc :as jdbc]
+    [honey.sql.helpers :as sql]
+    ))
 
 (def budget-limits-base-query
   (-> (sql/select :procurement_budget_limits.*)
@@ -13,21 +20,21 @@
 (defn get-budget-limits
   [context _ value]
   (let [main_category_id (:id value)]
-    (jdbc/query
+    (jdbc/execute!
       (-> context
           :request
           :tx)
       (-> budget-limits-base-query
-          (sql/merge-where [:= :procurement_budget_limits.main_category_id
+          (sql/where [:= :procurement_budget_limits.main_category_id
                             main_category_id])
-          sql/format))))
+          sql-format))))
 
 (defn insert-budget-limit!
   [tx bl]
   (jdbc/execute! tx
                  (-> (sql/insert-into :procurement_budget_limits)
                      (sql/values [bl])
-                     sql/format)))
+                     sql-format)))
 
 (defn delete-budget-limit!
   [tx bl]
@@ -36,7 +43,7 @@
     (-> (sql/delete-from [:procurement_budget_limits :pbl])
         (sql/where [:and [:= :pbl.main_category_id (:main_category_id bl)]
                     [:= :pbl.budget_period_id (:budget_period_id bl)]])
-        sql/format)))
+        sql-format)))
 
 (defn update-budget-limits!
   [tx bls]
