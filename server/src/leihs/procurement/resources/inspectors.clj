9a7edb9ddcd0d7d3c8f5a86adfb7995a68c5bdce
mradl <manuel.radl@zhdk.ch>
2023-11-21 14:42:31 +0100

    change v14

--- server/src/leihs/procurement/resources/inspectors.clj
+++ server/src/leihs/procurement/resources/inspectors.clj
@@ -1,34 +1,42 @@
 (ns leihs.procurement.resources.inspectors
-  (:require [clojure.java.jdbc :as jdbc]
+  (:require 
+    ;[clojure.java.jdbc :as jdbc]
+    ;        [leihs.procurement.utils.sql :as sql]
+
+    [honey.sql :refer [format] :rename {format sql-format}]
+    [leihs.core.db :as db]
+    [next.jdbc :as jdbc]
+    [honey.sql.helpers :as sql]
+    
             [leihs.procurement.resources.users :refer [users-base-query]]
-            [leihs.procurement.utils.sql :as sql]))
+    ))
 
 (defn get-inspectors
   [context _ value]
-  (jdbc/query (-> context
+  (jdbc/execute! (-> context
                   :request
                   :tx)
               (-> users-base-query
-                  (sql/merge-where
+                  (sql/where
                     [:in :users.id
                      (-> (sql/select :pci.user_id)
                          (sql/from [:procurement_category_inspectors :pci])
-                         (sql/merge-where [:= :pci.category_id (:id value)]))])
-                  sql/format)))
+                         (sql/where [:= :pci.category_id (:id value)]))])
+                  sql-format)))
 
 (defn delete-inspectors-for-category-id!
   [tx c-id]
   (jdbc/execute! tx
                  (-> (sql/delete-from [:procurement_category_inspectors :pci])
-                     (sql/merge-where [:= :pci.category_id c-id])
-                     sql/format)))
+                     (sql/where [:= :pci.category_id c-id])
+                     sql-format)))
 
 (defn insert-inspectors!
   [tx row-maps]
   (jdbc/execute! tx
                  (-> (sql/insert-into :procurement_category_inspectors)
                      (sql/values row-maps)
-                     sql/format)))
+                     sql-format)))
 
 (defn update-inspectors!
   [tx c-id u-ids]
