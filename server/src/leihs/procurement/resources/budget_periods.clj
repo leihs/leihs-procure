50e2cfcaa058e5bf8511d3b345ce5f76420ddbd1
mradl <manuel.radl@zhdk.ch>
2023-11-21 14:19:28 +0100

    change v8

--- server/src/leihs/procurement/resources/budget_periods.clj
+++ server/src/leihs/procurement/resources/budget_periods.clj
@@ -1,10 +1,18 @@
 (ns leihs.procurement.resources.budget-periods
   (:require [clojure.tools.logging :as log]
             [clj-time.format :as time-format]
-            [clojure.java.jdbc :as jdbc]
+    
+            ;[clojure.java.jdbc :as jdbc]
+            ;[leihs.procurement.utils.sql :as sql]
+
+    [honey.sql :refer [format] :rename {format sql-format}]
+    [leihs.core.db :as db]
+    [next.jdbc :as jdbc]
+    [honey.sql.helpers :as sql]
+    
             [com.walmartlabs.lacinia [resolve :as resolve]]
             [leihs.procurement.resources.budget-period :as budget-period]
-            [leihs.procurement.utils.sql :as sql]))
+    ))
 
 (def budget-periods-base-query
   (-> (sql/select :procurement_budget_periods.*)
@@ -14,28 +22,28 @@
 (defn budget-periods-query
   [args]
   (cond-> budget-periods-base-query
-    (:id args) (sql/merge-where [:in :procurement_budget_periods.id
+    (:id args) (sql/where [:in :procurement_budget_periods.id
                                  (:id args)])
     (-> args
         :whereRequestsCanBeMovedTo
         empty?
         not)
-      (sql/merge-where [:< :current_date
+      (sql/where [:< :current_date
                         :procurement_budget_periods.end_date])))
 
 (defn get-budget-periods
   ([tx ids]
-   (jdbc/query tx
+   (jdbc/execute! tx
                (-> budget-periods-base-query
-                   (sql/merge-where [:in :procurement_budget_periods.id ids])
-                   sql/format)))
+                   (sql/where [:in :procurement_budget_periods.id ids])
+                   sql-format)))
   ([context args _]
    (if (= (:id args) [])
      []
      (->> args
           budget-periods-query
-          sql/format
-          (jdbc/query (-> context
+          sql-format
+          (jdbc/execute! (-> context
                           :request
                           :tx))))))
 
@@ -43,8 +51,8 @@
   [tx ids]
   (jdbc/execute! tx
                  (-> (sql/delete-from [:procurement_budget_periods :pbp])
-                     (sql/merge-where [:not-in :pbp.id ids])
-                     sql/format)))
+                     (sql/where [:not-in :pbp.id ids])
+                     sql-format)))
 
 (defn update-budget-periods!
   [context args value]
