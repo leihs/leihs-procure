207870192654b2a6f0dd474bd04b2d7112e29bd1
mradl <manuel.radl@zhdk.ch>
2023-11-21 14:23:35 +0100

    change v10

--- server/src/leihs/procurement/resources/buildings.clj
+++ server/src/leihs/procurement/resources/buildings.clj
@@ -1,22 +1,30 @@
 (ns leihs.procurement.resources.buildings
-  (:require [clojure.java.jdbc :as jdbc]
-            [leihs.procurement.utils.sql :as sql]))
+  (:require 
+    ;[clojure.java.jdbc :as jdbc]
+    ;        [leihs.procurement.utils.sql :as sql]
+
+    [honey.sql :refer [format] :rename {format sql-format}]
+    [leihs.core.db :as db]
+    [next.jdbc :as jdbc]
+    [honey.sql.helpers :as sql]
+    
+    ))
 
 (def general-id "abae04c5-d767-425e-acc2-7ce04df645d1")
 
 (def buildings-base-query
   (-> (sql/select :buildings.*)
       (sql/from :buildings)
-      (sql/order-by [(sql/call := general-id :id) :desc] [:name :asc])))
+      (sql/order-by [(:= general-id :id) :desc] [:name :asc])))
 
 (defn buildings-query
   [args]
   (let [id (:id args)]
-    (cond-> buildings-base-query id (sql/merge-where [:= :buildings.id id]))))
+    (cond-> buildings-base-query id (sql/where [:= :buildings.id id]))))
 
 (defn get-buildings
   [context args _]
-  (jdbc/query (-> context
+  (jdbc/execute! (-> context
                   :request
                   :tx)
-              (sql/format (buildings-query args))))
+              (sql-format (buildings-query args))))
