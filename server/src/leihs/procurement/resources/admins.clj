f0d74df2ddcfd03dbcf4f5be2ccc32e585c18ae3
mradl <manuel.radl@zhdk.ch>
2023-11-21 13:55:15 +0100

    change v4

--- server/src/leihs/procurement/resources/admins.clj
+++ server/src/leihs/procurement/resources/admins.clj
@@ -1,39 +1,64 @@
 (ns leihs.procurement.resources.admins
-  (:require [clojure.java.jdbc :as jdbc]
-            [leihs.procurement.resources.user :as user]
-            [leihs.procurement.resources.users :refer [sql-order-users]]
-            [leihs.procurement.utils.sql :as sql]))
+    (:require
+
+      [honey.sql :refer [format] :rename {format sql-format}]
+      [leihs.core.db :as db]
+      [next.jdbc :as jdbc]
+      [honey.sql.helpers :as sql]
+
+      ;        [leihs.procurement.utils.sql :as sql]
+      ;[clojure.java.jdbc :as jdbc]
+
+      [leihs.procurement.resources.user :as user]
+      [leihs.procurement.resources.users :refer [sql-order-users]]
+      ))
 
 (def admins-base-query
   (-> (sql/select :users.*)
       (sql/from :users)
-      (sql/merge-where [:in :users.id
-                        (-> (sql/select :procurement_admins.user_id)
-                            (sql/from :procurement_admins))])
+      (sql/where [:in :users.id
+                  (-> (sql/select :procurement_admins.user_id)
+                      (sql/from :procurement_admins))])
       sql-order-users))
 
 (defn get-admins
-  [context _ _]
-  (jdbc/query (-> context
-                  :request
-                  :tx)
-              (sql/format admins-base-query)))
+      [context _ _]
+      (jdbc/execute! (-> context
+                         :request
+                         :tx)
+                     (sql-format admins-base-query)))
 
-(defn delete-all [tx] (jdbc/delete! tx :procurement_admins []))
+(defn delete-all [tx]                                       ;; TODO
+      ;(jdbc/delete! tx :procurement_admins [])
+
+      (->>
+        (sql/delete :procurement_admins)
+        sql-format
+        (jdbc/execute! tx))
+      )
 
 (defn update-admins!
-  [context args value]
-  (let [tx (-> context
-               :request
-               :tx)]
-    (delete-all tx)
-    (doseq [d (:input_data args)] (jdbc/insert! tx :procurement_admins d))
-    (let [admins (get-admins context args value)]
-      (map #(conj %
-                  {:user (->> %
-                              :user_id
-                              (user/get-user-by-id tx))})
-        admins))))
+      [context args value]
+      (let [tx (-> context
+                   :request
+                   :tx)]
+           (delete-all tx)
+           (doseq [d (:input_data args)]
+                  ;(jdbc/insert! tx :procurement_admins d)         ;; TODO
+                  (->> (sql/insert-into :procurement_admins)
+                      (sql/set d)
+                       sql-format
+                      (jdbc/execute! tx)
+                      )
+
+
+                  )
+           (let [admins (get-admins context args value)]
+                (map #(conj %
+                            {:user (->> %
+                                        :user_id
+                                        (user/get-user-by-id tx))})
+                     admins))))
 
 ;#### debug ###################################################################
 
