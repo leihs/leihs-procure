07583ce43d8e11997974e9840dcc652427a47fc7
mradl <manuel.radl@zhdk.ch>
2023-11-21 15:16:00 +0100

    change v22

--- server/src/leihs/procurement/resources/request_helpers.clj
+++ server/src/leihs/procurement/resources/request_helpers.clj
@@ -1,11 +1,20 @@
 (ns leihs.procurement.resources.request-helpers
-  (:require [clojure.java.jdbc :as jdbc]
+  (:require 
             [clojure.tools.logging :as log]
-            [leihs.procurement.utils.sql :as sql]))
+    
+    ;[clojure.java.jdbc :as jdbc]
+            [leihs.procurement.utils.sql :as sqlp]
+
+    [honey.sql :refer [format] :rename {format sql-format}]
+    [leihs.core.db :as db]
+    [next.jdbc :as jdbc]
+    [honey.sql.helpers :as sql]
+    
+    ))
 
 (defn join-and-nest-suppliers
   [sqlmap]
-  (sql/join-and-nest sqlmap
+  (sqlp/join-and-nest sqlmap
                      :suppliers
                      [:= :suppliers.id :procurement_requests.supplier_id]
                      :supplier))
@@ -14,44 +23,44 @@
   [sqlmap]
   (->
     sqlmap
-    (sql/merge-select
-      (sql/raw
+    (sql/select
+      (:raw
         (str
           "row_to_json(procurement_categories)::jsonb" " || "
           "jsonb_build_object('main_category', row_to_json(procurement_main_categories))"
             " AS category")))
-    (sql/merge-join :procurement_categories
+    (sql/join :procurement_categories
                     [:= :procurement_categories.id
                      :procurement_requests.category_id])
-    (sql/merge-join :procurement_main_categories
+    (sql/join :procurement_main_categories
                     [:= :procurement_main_categories.id
                      :procurement_categories.main_category_id])))
 
-(defn join-and-nest-models [sqlmap] (sql/select-nest sqlmap :models :model))
+(defn join-and-nest-models [sqlmap] (sqlp/select-nest sqlmap :models :model))
 
 (defn join-and-nest-organizations
   [sqlmap]
   (->
     sqlmap
-    (sql/merge-select
-      (sql/raw
+    (sql/select
+      (:raw
         (str
           "row_to_json(procurement_organizations)::jsonb" " || "
           "jsonb_build_object('department', row_to_json(procurement_departments))"
             " AS organization")))
-    (sql/merge-join :procurement_organizations
+    (sql/join :procurement_organizations
                     [:= :procurement_organizations.id
                      :procurement_requests.organization_id])
-    (sql/merge-join [:procurement_organizations :procurement_departments]
+    (sql/join [:procurement_organizations :procurement_departments]
                     [:= :procurement_departments.id
                      :procurement_organizations.parent_id])))
 
 (defn join-and-nest-budget-periods
   [sqlmap]
   (-> sqlmap
-      (sql/select-nest :procurement_budget_periods_2 :budget_period)
-      (sql/merge-join
-        [(-> (sql/select :* [(sql/call :> :current_date :end_date) :is_past])
+      (sqlp/select-nest :procurement_budget_periods_2 :budget_period)
+      (sql/join
+        [(-> (sql/select :* [( :> :current_date :end_date) :is_past])
              (sql/from :procurement_budget_periods))
          :procurement_budget_periods_2]
         [:= :procurement_budget_periods_2.id
@@ -59,7 +68,7 @@
 
 (defn join-and-nest-templates
   [sqlmap]
-  (sql/join-and-nest sqlmap
+  (sqlp/join-and-nest sqlmap
                      :procurement_templates
                      [:= :procurement_templates.id
                       :procurement_requests.template_id]
@@ -68,17 +77,17 @@
 (defn join-and-nest-rooms
   [sqlmap]
   (-> sqlmap
-      (sql/merge-select
-        (sql/raw (str "row_to_json(rooms)::jsonb" " || "
+      (sql/select
+        (:raw (str "row_to_json(rooms)::jsonb" " || "
                       "jsonb_build_object('building', row_to_json(buildings))"
                         " AS room")))
-      (sql/merge-join :rooms [:= :rooms.id :procurement_requests.room_id])
-      (sql/merge-join :buildings [:= :buildings.id :rooms.building_id])))
+      (sql/join :rooms [:= :rooms.id :procurement_requests.room_id])
+      (sql/join :buildings [:= :buildings.id :rooms.building_id])))
 
 
 (defn join-and-nest-users
   [sqlmap]
-  (sql/join-and-nest sqlmap
+  (sqlp/join-and-nest sqlmap
                      :users
                      [:= :users.id :procurement_requests.user_id]
                      :user))
