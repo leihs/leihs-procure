f3f71622154c1d10403638dbea38f81ffc2df0fb
mradl <manuel.radl@zhdk.ch>
2023-11-27 16:32:02 +0100

    add comment

--- server/src/leihs/procurement/resources/requests.clj
+++ server/src/leihs/procurement/resources/requests.clj
@@ -5,7 +5,7 @@
             [leihs.procurement.permissions [request-helpers :as request-perms]
              [requests :as requests-perms] [user :as user-perms]]
 
-                [taoensso.timbre :refer [debug info warn error spy]]
+            [taoensso.timbre :refer [debug info warn error spy]]
 
 
             [leihs.procurement.resources.request :as request]
@@ -23,7 +23,7 @@
         ; NOTE: models are joined in the base-query already
         (sql/merge-where
           [:or ["~~*" :buildings.name term-percent]
-          ;  ["~~*" :procurement_requests.id term-percent]
+           ;  ["~~*" :procurement_requests.id term-percent]
            ["~~*" :procurement_requests.short_id term-percent]
            ["~~*" :procurement_requests.article_name term-percent]
            ["~~*" :procurement_requests.article_number term-percent]
@@ -46,11 +46,11 @@
         budget-period-id (:budget_period_id arguments)
         organization-id (:organization_id arguments)
         priority (some->> arguments
-                          :priority
-                          (map request/to-name-and-lower-case))
+                   :priority
+                   (map request/to-name-and-lower-case))
         inspector-priority (some->> arguments
-                                    :inspector_priority
-                                    (map request/to-name-and-lower-case))
+                             :inspector_priority
+                             (map request/to-name-and-lower-case))
         requested-by-auth-user (:requested_by_auth_user arguments)
         state (:state arguments)
         search-term (:search arguments)
@@ -62,38 +62,38 @@
         start-sqlmap (-> (request/requests-base-query-with-state advanced-user?)
                          request-helpers/join-and-nest-associated-resources)]
     (cond-> start-sqlmap
-      id (sql/merge-where [:in :procurement_requests.id id])
-      ; short_id (sql/merge-where [:in :procurement_requests.short_id short_id])
-      category-id (-> (sql/merge-where [:in :procurement_requests.category_id
-                                        category-id])
-                      (sql/merge-where-false-if-empty category-id))
-      budget-period-id (-> (sql/merge-where
-                             [:in :procurement_requests.budget_period_id
-                              budget-period-id])
-                           (sql/merge-where-false-if-empty budget-period-id))
-      organization-id (-> (sql/merge-where
-                            [:in :procurement_requests.organization_id
-                             organization-id])
-                          (sql/merge-where-false-if-empty organization-id))
-      priority (-> (sql/merge-where [:in :procurement_requests.priority
-                                     priority])
-                   (sql/merge-where-false-if-empty priority))
-      inspector-priority
-      (-> (sql/merge-where [:in :procurement_requests.inspector_priority
-                            inspector-priority])
-          (sql/merge-where-false-if-empty inspector-priority))
-      state (-> (sql/merge-where
-                  (request/get-where-conds-for-states state advanced-user?))
-                (sql/merge-where-false-if-empty state))
-      order-status (-> (sql/merge-where [:in :procurement_requests.order_status
-                                         (map #(sql/call :cast % :order_status_enum) order-status)])
-                       (sql/merge-where-false-if-empty order-status))
-      requested-by-auth-user (sql/merge-where [:= :procurement_requests.user_id
-                                               (-> context
-                                                   :request
-                                                   :authenticated-entity
-                                                   :user_id)])
-      search-term (search-query search-term))))
+            id (sql/merge-where [:in :procurement_requests.id id])
+            ; short_id (sql/merge-where [:in :procurement_requests.short_id short_id])
+            category-id (-> (sql/merge-where [:in :procurement_requests.category_id
+                                              category-id])
+                            (sql/merge-where-false-if-empty category-id))
+            budget-period-id (-> (sql/merge-where
+                                   [:in :procurement_requests.budget_period_id
+                                    budget-period-id])
+                                 (sql/merge-where-false-if-empty budget-period-id))
+            organization-id (-> (sql/merge-where
+                                  [:in :procurement_requests.organization_id
+                                   organization-id])
+                                (sql/merge-where-false-if-empty organization-id))
+            priority (-> (sql/merge-where [:in :procurement_requests.priority
+                                           priority])
+                         (sql/merge-where-false-if-empty priority))
+            inspector-priority
+            (-> (sql/merge-where [:in :procurement_requests.inspector_priority
+                                  inspector-priority])
+                (sql/merge-where-false-if-empty inspector-priority))
+            state (-> (sql/merge-where
+                        (request/get-where-conds-for-states state advanced-user?))
+                      (sql/merge-where-false-if-empty state))
+            order-status (-> (sql/merge-where [:in :procurement_requests.order_status
+                                               (map #(sql/call :cast % :order_status_enum) order-status)])
+                             (sql/merge-where-false-if-empty order-status))
+            requested-by-auth-user (sql/merge-where [:= :procurement_requests.user_id
+                                                     (-> context
+                                                         :request
+                                                         :authenticated-entity
+                                                         :user_id)])
+            search-term (search-query search-term))))
 
 (defn get-requests
   [context arguments value]
@@ -107,28 +107,31 @@
         auth-entity (:authenticated-entity ring-request)
         p (println ">get-requests>> ???")
         ;p  (spy context)
-        query (as-> context <>
-                ;(requests-query-map <> arguments value)
-                ;(requests-perms/apply-scope tx <> auth-entity)
-                (sql/format <>))
+
+
+
+        query (let [query (as-> context <>
+                            (do (println ">o> 1-After requests-query-map:")
+                                ;(do (println ">o> 1-After requests-query-map:" <>)
+                                (requests-query-map <> arguments value))
+
+                            ;>o> 2-After apply-scope: {:select (nil [(:case [:= :procurement_requests.approved_quantity nil] NEW [:>= :procurement_requests.approved_quantity :procurement_requests.requested_quantity] APPROVED [:and [:< :procurement_requests.approved_quantity :procurement_requests.requested_quantity] [:> :procurement_requests.approved_quantity 0]] PARTIALLY_APPROVED [:= :procurement_requests.approved_quantity 0] DENIED) :state] [#sql/call [:row_to_json :procurement_budget_periods_2] :budget_period] nil [#sql/call [:row_to_json :models] :model] nil [#sql/call [:row_to_json :procurement_templates] :template] nil [#sql/call [:row_to_json :suppliers] :supplier] [#sql/call [:row_to_json :users] :user]), :from [:procurement_requests], :left-join (:models [:= :models.id :procurement_requests.model_id] :procurement_templates [:= :procurement_templates.id :procurement_requests.template_id] :suppliers [:= :suppliers.id :procurement_requests.supplier_id] :users [:= :users.id :procurement_requests.user_id]), :order-by [nil], :join [:procurement_budget_periods [:= :procurement_budget_periods.id :procurement_requests.budget_period_id] [{:select [:* [:end_date :is_past]], :from [:procurement_budget_periods]} :procurement_budget_periods_2] [:= :procurement_budget_periods_2.id :procurement_requests.budget_period_id] :procurement_categories [:= :procurement_categories.id :procurement_requests.category_id] :procurement_main_categories [:= :procurement_main_categories.id :procurement_categories.main_category_id] :procurement_organizations [:= :procurement_organizations.id :procurement_requests.organization_id] [:procurement_organizations :procurement_departments] [:= :procurement_departments.id :procurement_organizations.parent_id] :rooms [:= :rooms.id :procurement_requests.room_id] :buildings [:= :buildings.id :rooms.building_id]], :where [:and [:in :procurement_requests.category_id [#uuid "7d5ba731-edd9-41ba-8773-7337d24c2327"]] [:in :procurement_requests.budget_period_id [#uuid "8b8fe440-cae5-4bf9-8048-d0ec2399faa1"]] [:in :procurement_requests.organization_id [#uuid "fb664326-a8ef-4556-af02-07d3127cd9ec"]] [:in :procurement_requests.priority (normal high)] [:in :procurement_requests.inspector_priority (low medium high mandatory)] [:or [:= :procurement_requests.approved_quantity nil] [:>= :procurement_requests.approved_quantity :procurement_requests.requested_quantity] [:and [:< :procurement_requests.approved_quantity :procurement_requests.requested_quantity] [:> :procurement_requests.approved_quantity 0]] [:= :procurement_requests.approved_quantity 0]]]}
+                            ;                                   ^  TODO:
+
+                            ; TODO/GOOD >o> 2-After apply-scope: {:select (#sql/raw DISTINCT ON (procurement_requests.id, concat(lower(coalesce(procurement_requests.article_name, '')), lower(coalesce(models.product, '')), lower(coalesce(models.version, '')))) procurement_requests.* [#sql/call [:case [:= :procurement_requests.approved_quantity nil] NEW [:>= :procurement_requests.approved_quantity :procurement_requests.requested_quantity] APPROVED [:and [:< :procurement_requests.approved_quantity :procurement_requests.requested_quantity] [:> :procurement_requests.approved_quantity 0]] PARTIALLY_APPROVED [:= :procurement_requests.approved_quantity 0] DENIED] :state] [#sql/call [:row_to_json :procurement_budget_periods_2] :budget_period] #sql/raw row_to_json(procurement_categories)::jsonb || jsonb_build_object('main_category', row_to_json(procurement_main_categories)) AS category [#sql/call [:row_to_json :models] :model] #sql/raw row_to_json(procurement_organizations)::jsonb || jsonb_build_object('department', row_to_json(procurement_departments)) AS organization [#sql/call [:row_to_json :procurement_templates] :template] #sql/raw row_to_json(rooms)::jsonb || jsonb_build_object('building', row_to_json(buildings)) AS room [#sql/call [:row_to_json :suppliers] :supplier] [#sql/call [:row_to_json :users] :user]), :from (:procurement_requests), :left-join (:models [:= :models.id :procurement_requests.model_id] :procurement_templates [:= :procurement_templates.id :procurement_requests.template_id] :suppliers [:= :suppliers.id :procurement_requests.supplier_id] :users [:= :users.id :procurement_requests.user_id]), :order-by (#sql/raw concat(lower(coalesce(procurement_requests.article_name, '')), lower(coalesce(models.product, '')), lower(coalesce(models.version, '')))), :join (:procurement_budget_periods [:= :procurement_budget_periods.id :procurement_requests.budget_period_id] [{:select (:* [#sql/call [:> :current_date :end_date] :is_past]), :from (:procurement_budget_periods)} :procurement_budget_periods_2] [:= :procurement_budget_periods_2.id :procurement_requests.budget_period_id] :procurement_categories [:= :procurement_categories.id :procurement_requests.category_id] :procurement_main_categories [:= :procurement_main_categories.id :procurement_categories.main_category_id] :procurement_organizations [:= :procurement_organizations.id :procurement_requests.organization_id] [:procurement_organizations :procurement_departments] [:= :procurement_departments.id :procurement_organizations.parent_id] :rooms [:= :rooms.id :procurement_requests.room_id] :buildings [:= :buildings.id :rooms.building_id]), :where [:and [:in :procurement_requests.category_id [7d5ba731-edd9-41ba-8773-7337d24c2327]] [:in :procurement_requests.budget_period_id [8b8fe440-cae5-4bf9-8048-d0ec2399faa1]] [:in :procurement_requests.organization_id [fb664326-a8ef-4556-af02-07d3127cd9ec]] [:in :procurement_requests.priority (normal high)] [:in :procurement_requests.inspector_priority (low medium high mandatory)] [:or [:= :procurement_requests.approved_quantity nil] [:>= :procurement_requests.approved_quantity :procurement_requests.requested_quantity] [:and [:< :procurement_requests.approved_quantity :procurement_requests.requested_quantity] [:> :procurement_requests.approved_quantity 0]] [:= :procurement_requests.approved_quantity 0]] [:in :procurement_requests.order_status (#sql/call [:cast not_processed :order_status_enum] #sql/call [:cast in_progress :order_status_enum] #sql/call [:cast procured :order_status_enum] #sql/call [:cast alternative_procured :order_status_enum] #sql/call [:cast not_procured :order_status_enum])]]}
+
+
+                            (do (println ">o> 2-After apply-scope:" <>)
+                                (requests-perms/apply-scope tx <> auth-entity))
+                            (do (println ">o> 3-After sql-format:" <>)
+                                (sql/format <>)))] query
+                                                   ;; Execute the query with `query`, for example using a PostgreSQL function.
+                                                   ;; Example: (execute-postgres-query tx query)
+                                                   )
 
         p (spy query)
 
-        query (as-> context <>
-                (requests-query-map <> arguments value)
-                ;(requests-perms/apply-scope tx <> auth-entity)
-                (sql/format <>))
-
-        p (spy query)
-
-        query (as-> context <>
-                (requests-query-map <> arguments value)
-                (requests-perms/apply-scope tx <> auth-entity)
-                (sql/format <>))
-
-        p (spy query)
-
-        p  (println ">>broken-query" (spy query))           ;;TODO: log broken query
+        p (println ">>broken-query" (spy query))            ;;TODO: log broken query
         ;p (throw "my-log-error")
         proc-requests (request/query-requests tx auth-entity query)]
     (->>
@@ -144,10 +147,10 @@
 (defn get-total-price-cents
   [tx sqlmap]
   (or (some->> sqlmap
-               sql/format
-               (jdbc/query tx)
-               first
-               :result)
+        sql/format
+        (jdbc/query tx)
+        first
+        :result)
       0))
 
 (defn- sql-sum
