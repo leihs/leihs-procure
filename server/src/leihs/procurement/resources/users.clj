afe130369199583aaccca4a937d59afa5c87b3e0
mradl <manuel.radl@zhdk.ch>
2023-12-11 10:44:18 +0100

    DEBUG: fixed(?) users::get-users / ilike issue / search

--- server/src/leihs/procurement/resources/users.clj
+++ server/src/leihs/procurement/resources/users.clj
@@ -1,5 +1,9 @@
 (ns leihs.procurement.resources.users
   (:require [clojure.java.jdbc :as jdbc]
+
+                [taoensso.timbre :refer [debug info warn error spy]]
+
+
             [clojure.string :as clj-str]
             [leihs.procurement.utils.sql :as sql]))
 
@@ -16,6 +20,9 @@
 
 (defn get-users
   [context args _]
+
+  (println ">o> users::get-users" get-users)
+
   (jdbc/query
     (-> context
         :request
@@ -27,7 +34,21 @@
           is-requester (:isRequester args)
           exclude-ids (:exclude_ids args)
           offset (:offset args)
-          limit (:limit args)]
+          limit (:limit args)
+
+          ]
+
+      (println ">o> >tocheck> search-term=" search-term)
+      (println ">o> >tocheck> term-parts=" term-parts)
+      (println ">o> >tocheck> is-requester=" is-requester)
+      (println ">o> >tocheck> exclude-ids=" exclude-ids)
+      (println ">o> >tocheck> offset=" offset)
+      (println ">o> >tocheck> limit=" limit)
+
+
+      (println ">o> >tocheck> merge-where1=" exclude-ids offset)
+      (println ">o> >tocheck> merge-where2=" term-parts)
+
       (-> (cond-> users-base-query is-requester
                   (sql/merge-join :procurement_requesters_organizations
                                   [:=
@@ -37,17 +58,23 @@
                   (sql/merge-where
                     (into [:and]
                           (map
-                            (fn [term-percent]
+
+                            (spy (fn [term-percent]
                               ["~~*"
                                (->> (sql/call :concat
                                               :users.firstname
                                               (sql/call :cast " " :varchar)
                                               :users.lastname)
                                     (sql/call :unaccent))
-                               (sql/call :unaccent term-percent)])
+                               (sql/call :unaccent term-percent)]))
+
                             term-parts)))
                     exclude-ids
                   (sql/merge-where [:not-in :users.id exclude-ids]) offset
                   (sql/offset offset) limit
                   (sql/limit limit))
-          sql/format))))
+          sql/format
+          spy
+          )
+
+      )))
