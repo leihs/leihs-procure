66f3753d6600354652fdc61450b9b1b8be3922c7
mradl <manuel.radl@zhdk.ch>
2023-12-12 09:35:55 +0100

    fixed(?) settings

--- server/src/leihs/procurement/resources/settings.clj
+++ server/src/leihs/procurement/resources/settings.clj
@@ -1,13 +1,16 @@
 (ns leihs.procurement.resources.settings
-  (:require 
+  (:require
     ;[clojure.java.jdbc :as jdbc]
-    ;        [leihs.procurement.utils.sql :as sql]
-
     [honey.sql :refer [format] :rename {format sql-format}]
-    [leihs.core.db :as db]
-    [next.jdbc :as jdbc]
+
     [honey.sql.helpers :as sql]
-    
+
+
+    [leihs.core.db :as db]
+    [leihs.procurement.utils.sql :as sqlo]
+    [next.jdbc :as jdbc]
+    [taoensso.timbre :refer [debug error info spy warn]]
+
     ))
 
 (def settings-base-query
@@ -33,13 +36,141 @@
         input-data (:input_data args)
         inspection-comments (->> input-data
                                  :inspection_comments
-                                 (map #( :cast % :text))    ;; TODO FIXME PRIO!!!
-                                 (cons :jsonb_build_array)
-                                 (apply ))
+                                 (map (fn [comment] [:cast comment :text]))
+                                 (cons :jsonb_build_array))
         settings (-> input-data
                      (assoc :inspection_comments inspection-comments))]
-    (jdbc/execute! tx
-                   (-> (sql/update :procurement_settings)
-                       (sql/set settings)
-                       sql-format))
-    (get-settings tx)))
+    (jdbc/execute-one! tx (-> (sql/update :procurement_settings)
+                              (sqlo/sset (spy settings))
+                              sql-format
+                              spy
+                              ))
+    (get-settings tx)
+
+    ))
+
+
+
+
+
+
+
+
+(comment
+
+  (let [
+        tx (db/get-ds-next)
+        request {:route-params {:user-id #uuid "c0777d74-668b-5e01-abb5-f8277baa0ea8"}
+                 :tx-next tx}
+
+        ;; master (old version)
+        ;inspection-comments (->> input-data
+        ;                         :inspection_comments
+        ;                         (map #(sql/call :cast % :text))
+        ;                         (cons :jsonb_build_array)
+        ;                         (apply sql/call))
+
+
+        input-data {:contact_url "https://intern.zhdk.ch/index.php?id=leihs_bedarfsermittlung", :inspection_comments ["Projektidee." "Projektauftrag muss erarbeitet werden (M20).abc222333"]}
+
+        inspection-comments (->> input-data
+                                 :inspection_comments
+                                 (map (fn [uuid-str] [:cast uuid-str :text]))
+                                 (cons :jsonb_build_array)
+                                 )
+
+        p (println "\n>o> inspection-comments1" inspection-comments)
+
+        settings (-> (spy input-data)
+                     (assoc :inspection_comments inspection-comments))
+
+        result (jdbc/execute-one! tx (-> (sql/update :procurement_settings)
+                                         (sqlo/sset settings)
+                                         sql-format
+                                         spy
+                                         ))
+
+        p (println "\nquery" (spy result))
+
+
+        p (spy (:next.jdbc/update-count result))            ;;1
+        p (spy (:update-count result))                      ;;nil
+
+        ]
+    )
+  )
+
+
+
+(comment
+
+  (let [
+        tx (db/get-ds-next)
+        request {:route-params {:user-id #uuid "c0777d74-668b-5e01-abb5-f8277baa0ea8"}
+                 :tx-next tx}
+
+        ;; master (old version)
+        ;inspection-comments (->> input-data
+        ;                         :inspection_comments
+        ;                         (map #(sql/call :cast % :text))
+        ;                         (cons :jsonb_build_array)
+        ;                         (apply sql/call))
+
+
+        input-data {:contact_url "https://intern.zhdk.ch/index.php?id=leihs_bedarfsermittlung", :inspection_comments ["Projektidee." "Projektauftrag muss erarbeitet werden (M20).abc222"]}
+
+        inspection-comments (->> input-data
+                                 :inspection_comments
+                                 ;(map #([:cast % :text]))
+                                 ;(cons :jsonb_build_array)
+                                 )
+
+        p (println "\n>o> inspection-comments1" inspection-comments)
+
+
+
+        ;inspection-comments #([:cast % :text] inspection-comments)
+
+        inspection-comments (map (fn [uuid-str] [:cast uuid-str :text]) inspection-comments)
+        p (println "\n>o> inspection-comments2a" inspection-comments)
+
+        inspection-comments (cons :jsonb_build_array inspection-comments)
+
+
+        ;inspection-comments (map inspection-comments)
+        ;p (println "\n>o> inspection-comments2b" inspection-comments)
+
+
+        ;inspection-comments [[inspection-comments ]]
+        ;inspection-comments [:cast inspection-comments :jsonb]
+        inspection-comments [inspection-comments]
+
+        p (println "\n>o> inspection-comments3" inspection-comments)
+
+
+        ;settings inspection-comments
+        p (println "\n>o> query" (sql-format inspection-comments))
+
+
+        settings (-> (spy input-data)
+                     (assoc :inspection_comments inspection-comments))
+        ;;p (println "\n>o> settings" settings)
+        ;;
+        ;;
+        result (jdbc/execute! tx
+                              (-> (sql/update :procurement_settings)
+                                  (sqlo/sset settings)
+                                  sql-format
+                                  spy
+                                  ))
+
+        ;>o> settings {:contact_url https://intern.zhdk.ch/index.php?id=leihs_bedarfsermittlung, :inspection_comments #sql/call [:jsonb_build_array #sql/call [:cast Projektidee. Projektauftrag muss erarbeitet werden (M20).abc222 :text]]}
+        ;[leihs.procurement.resources.settings:?] - (sql/format (sql/sset (sql/update :procurement_settings) settings)) => ["UPDATE procurement_settings SET contact_url = ?, inspection_comments = jsonb_build_array(CAST(? AS text)) " "https://intern.zhdk.ch/index.php?id=leihs_bedarfsermittlung" "Projektidee. Projektauftrag muss erarbeitet werden (M20).abc222"]
+        ;result => (1)
+
+        ;result => (1)
+        p (println "\nquery" (spy result))
+        ]
+    )
+  )
+
