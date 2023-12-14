c481277e047bc7aae403cf70c84626b32aa9c36c
mradl <manuel.radl@zhdk.ch>
2023-12-14 10:00:34 +0100

    dev: additional constraint to avoid fk-exception v2

--- server/src/leihs/procurement/resources/organizations.clj
+++ server/src/leihs/procurement/resources/organizations.clj
@@ -104,9 +104,9 @@
   (println "------ start DELETE-2 -----------------")
   (spy (jdbc/execute! tx (->
 
-                           ;(sql/select :*)
-                           (sql/select :%count.*)
-                           (sql/from [:procurement_organizations :po1] [:procurement_requesters_organizations req]) ;; fixme TODO cause
+                           (sql/select :*)
+                           ;(sql/select :%count.*)
+                           (sql/from [:procurement_organizations :po1] [:procurement_requesters_organizations :req]) ;; fixme TODO cause
 
 
                            (sql/where :and [:= :po1.parent_id nil]
@@ -119,10 +119,11 @@
                            spy
                            )))
 
+  (println ">>>>>>>>> want to delete")
   (spy (jdbc/execute! tx (->
 
-                           (sql/select :*)
-                           ;(sql/select :%count.*)
+                           ;(sql/select :*)
+                           (sql/select :%count.*)
                            (sql/from [:procurement_organizations :po1]) ;; fixme TODO cause
 
 
@@ -134,7 +135,56 @@
                            sql-format
                            spy
                            )))
+
+  (println ">>>>>>>>> todo")
+  (spy (jdbc/execute! tx (->
+
+                           (sql/select :po1.id :po1.name :po1.parent_id)
+                           ;(sql/select :%count.*)
+                           (sql/from [:procurement_organizations :po1]) ;; fixme TODO cause
+
+
+                           (sql/where :and [:= :po1.parent_id nil]
+
+                                      ;WHERE procurement_requesters_organizations.organization_id = procurement_organizations.id
+
+
+                                      ;[:not [:exists (-> (sql/select true)
+                                      ;                   (sql/from [:procurement_organizations :po2])
+                                      ;                   (sql/inner-join :procurement_requesters_organizations [:= :po2.id :procurement_requesters_organizations.organization_id])
+                                      ;                   (sql/where [:= :po2.parent_id :po1.id] [:= :po1.parent_id nil]))]]
+
+                                      [:not [:exists (-> (sql/select true)
+                                                         (sql/from [:procurement_requesters_organizations :po3])
+                                                         ;(sql/inner-join :procurement_requesters_organizations [:= :po3.id :procurement_requesters_organizations.organization_id])
+                                                         ;(sql/where [:= :po3.organization_id :po1.parent_id])
+                                                         (sql/where :and [:or [:= :po3.organization_id :po1.parent_id]
+                                                                    [:= :po3.organization_id :po1.id]] [[:= :po1.parent_id nil]]
+                                                                    )
+                                                         )]]
+                                      )
+
+                           ;[:in :po1.parent_id nil]
+                           ;)
+                           ;(sql/where
+                           ;  ;:and [:= :po1.parent_id nil]
+                           ;           [:not [:exists (-> (sql/select true)
+                           ;                              (sql/from [:procurement_organizations :po2])
+                           ;                              (sql/inner-join :procurement_requesters_organizations [:= :po2.id :procurement_requesters_organizations.organization_id])
+                           ;                              (sql/where [:= :po2.parent_id :po1.parent_id]))]]
+                           ;           )
+
+                           sql-format
+                           spy)))
   (println "------ end DELETE-2 -----------------")
+
+
+  ;procurement_requesters_organizations as org
+  ;on po1.id =org.organization_id
+
+  ;(println ">>>>>>>>>>> sleeep now")
+  ;(Thread/sleep 10000)
+
 
 
   ;["DELETE FROM procurement_organizations AS po1 WHERE (po1.parent_id IS NULL) AND NOT EXISTS (SELECT TRUE FROM procurement_organizations AS po2 WHERE po2.parent_id = po1.id)"]
@@ -144,19 +194,30 @@
                              (sql/where :and [:= :po1.parent_id nil]
                                         [:not [:exists (-> (sql/select true)
                                                            (sql/from [:procurement_organizations :po2])
-                                                           (sql/where [:= :po2.parent_id :po1.id]))]])
+                                                           (sql/where [:= :po2.parent_id :po1.id]))]]
+
+                                        ;[:not [:exists (-> (sql/select true)
+                                        ;                   (sql/from [:procurement_organizations :po2])
+                                        ;                   (sql/where [:= :po2.parent_id :po1.parent_id]))]]
+
+                                        [:not [:exists (-> (sql/select true)
+                                                           (sql/from [:procurement_requesters_organizations :pro])
+                                                           (sql/where [:= :pro.organization_id :po1.id]))]]
 
 
-                             ;(sql/where [:= :po1.parent_id nil])
-                             ;(sql/where [:not [:exists (-> (sql/select true)
-                             ;                              (sql/from [:procurement_organizations :po2])
-                             ;                              (sql/where [:= :po2.parent_id :po1.id]))]])
+                                        )
 
 
+                             ;(sql/where
+                             ;           [:not [:in :po1.id (-> (sql/select :po3.id)
+                             ;                                  (sql/from [:procurement_organizations :po3])
+                             ;                                  (sql/inner-join :procurement_requesters_organizations [:= :po3.id :procurement_requesters_organizations.organization_id])
+                             ;                                  (sql/where [:<> :po3.parent_id nil])
+                             ;                                  ;(sql/where [:= :po3.parent_id :po1.id])
+                             ;                                  )]])
 
                              sql-format
-                             spy
-                             ))))
+                             spy))))
 
 
 
