db49de226f19da4a18d7f4c1afed7f6e0b0f3502
mradl <manuel.radl@zhdk.ch>
2023-12-16 09:57:01 +0100

    debug logs budget-perios

--- server/src/leihs/procurement/resources/budget_periods.clj
+++ server/src/leihs/procurement/resources/budget_periods.clj
@@ -2,9 +2,29 @@
   (:require [clojure.tools.logging :as log]
             [clj-time.format :as time-format]
             [clojure.java.jdbc :as jdbc]
+
+                [taoensso.timbre :refer [debug info warn error spy]]
+
             [com.walmartlabs.lacinia [resolve :as resolve]]
             [leihs.procurement.resources.budget-period :as budget-period]
             [leihs.procurement.utils.sql :as sql]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 (def budget-periods-base-query
   (-> (sql/select :procurement_budget_periods.*)
@@ -13,38 +33,70 @@
 
 (defn budget-periods-query
   [args]
-  (cond-> budget-periods-base-query
-    (:id args) (sql/merge-where [:in :procurement_budget_periods.id
-                                 (:id args)])
-    (-> args
-        :whereRequestsCanBeMovedTo
-        empty?
-        not)
-      (sql/merge-where [:< :current_date
-                        :procurement_budget_periods.end_date])))
+  (spy (cond-> budget-periods-base-query
+          (:id args) (sql/merge-where [:in :procurement_budget_periods.id
+                                       (:id args)])
+          (spy (-> args
+              :whereRequestsCanBeMovedTo
+              empty?
+              not))
+          (sql/merge-where [:< :current_date
+                            :procurement_budget_periods.end_date]))))
+
+
+
+
 
 (defn get-budget-periods
   ([tx ids]
-   (jdbc/query tx
+   (spy (jdbc/query tx
                (-> budget-periods-base-query
                    (sql/merge-where [:in :procurement_budget_periods.id ids])
-                   sql/format)))
+                   sql/format))))
   ([context args _]
-   (if (= (:id args) [])
+   (if (spy (= (:id args) []))
      []
-     (->> args
+     (spy (->> args
           budget-periods-query
           sql/format
           (jdbc/query (-> context
                           :request
-                          :tx))))))
+                          :tx)))))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 (defn delete-budget-periods-not-in!
   [tx ids]
-  (jdbc/execute! tx
+  (spy (jdbc/execute! tx
                  (-> (sql/delete-from [:procurement_budget_periods :pbp])
                      (sql/merge-where [:not-in :pbp.id ids])
-                     sql/format)))
+                     sql/format))))
+
+
+
+
+
+
+
+
+
+
 
 (defn update-budget-periods!
   [context args value]
@@ -54,21 +106,25 @@
         bps (:input_data args)]
     (loop [[bp & rest-bps] bps
            bp-ids []]
-      (if bp
-        (let [bp-with-dates (-> bp
+      (if (spy bp)
+        (let [bp-with-dates  (spy (-> bp                    ;;  => {:id nil, :name "new_bp", :inspection_start_date #clj-time/date-time "2025-06-01T00:00:00.000Z", :end_date #clj-time/date-time "2025-12-01T00:00:00.000Z"}
                                 (update :inspection_start_date
-                                        time-format/parse)
-                                (update :end_date time-format/parse))]
+                                           time-format/parse)
+                                (update :end_date time-format/parse)))]
           (do
-            (if (:id bp-with-dates)
-              (budget-period/update-budget-period! tx bp-with-dates)
-              (budget-period/insert-budget-period! tx
-                                                   (dissoc bp-with-dates :id)))
+            (if (spy (:id bp-with-dates))
+              (spy (budget-period/update-budget-period! tx bp-with-dates)) ;; nil
+              (spy (budget-period/insert-budget-period! tx (dissoc bp-with-dates :id)))) ;; => (1)
             (let [bp-id (or (:id bp-with-dates)
                             (-> bp-with-dates
                                 (dissoc :id)
                                 (->> (budget-period/get-budget-period tx))
                                 :id))]
-              (recur rest-bps (conj bp-ids bp-id)))))
-        (do (delete-budget-periods-not-in! tx bp-ids)
-            (get-budget-periods context args value))))))
+             (recur rest-bps (conj bp-ids bp-id))
+
+              )))
+        (do (spy (delete-budget-periods-not-in! tx bp-ids)) ;; (delete-budget-periods-not-in! tx bp-ids) => (1)
+            (spy (get-budget-periods context args value))
+            ;;({:id #uuid "bc517cb6-8efb-44a7-be05-032a309e4504", :name "new_bp", :inspection_start_date #time/instant "2025-06-01T00:00:00Z", :end_date #time/instant "2025-12-01T00:00:00Z", :created_at #time/instant "2023-12-16T08:50:52.351065Z", :updated_at #time/instant "2023-12-16T08:50:52.351065Z"} {:id #uuid "a50ba927-bc61-4403-a5e3-03fd9e38d16f", :name "bp_1_new_name", :inspection_start_date #time/instant "2024-06-01T00:00:00Z", :end_date #time/instant "2024-12-01T00:00:00Z", :created_at #time/instant "2023-12-16T08:50:50.322549Z", :updated_at #time/instant "2023-12-16T08:50:50.322549Z"})
+
+            )))))
