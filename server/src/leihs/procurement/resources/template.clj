03cb06bb8a5feca1262d8f10e26b87e0d0a9f1d0
mradl <manuel.radl@zhdk.ch>
2023-12-16 19:13:10 +0100

    ./spec/graphql/templates_spec.rb:6

--- server/src/leihs/procurement/resources/template.clj
+++ server/src/leihs/procurement/resources/template.clj
@@ -1,6 +1,9 @@
 (ns leihs.procurement.resources.template
   (:require [clojure.java.jdbc :as jdbc]
             [clojure.data :refer [diff]]
+
+                [taoensso.timbre :refer [debug info warn error spy]]
+
             [leihs.core.core :refer [raise]]
             [leihs.procurement.utils.sql :as sql]
             [taoensso.timbre :refer [debug info warn error]]))
@@ -15,67 +18,65 @@
   [tx id]
   (println ">o> tocheck ??? get-template-by-id" id)
 
-  (-> templates-base-query
-      (sql/merge-where [:= :procurement_templates.id id])
-      sql/format
-      (->> (jdbc/query tx))
-      first))
+  (spy (-> templates-base-query
+           (sql/merge-where [:= :procurement_templates.id id])
+           sql/format
+           (->> (jdbc/query tx))
+           first)))
 
 (defn insert-template!
   [tx tmpl]
-  (jdbc/execute! tx
-                 (-> (sql/insert-into :procurement_templates)
-                     (sql/values [tmpl])
-                     sql/format)))
+  (spy (jdbc/execute! tx
+                      (-> (sql/insert-into :procurement_templates)
+                          (sql/values [tmpl])
+                          sql/format))))
 
 (defn validate-update-attributes [tx tmpl]
-  (let [req-exist? (-> (sql/select :%count.*)
-                       (sql/from :procurement_requests)
-                       (sql/where [:= :template_id (:id tmpl)])
-                       sql/format
-                       (->> (jdbc/query tx))
-                       first :count (> 0))]
-    (if req-exist?
+  (let [req-exist? (spy (-> (sql/select :%count.*)
+                            (sql/from :procurement_requests)
+                            (sql/where [:= :template_id (:id (spy tmpl))])
+                            sql/format
+                            (->> (jdbc/query tx))
+                            first :count (> 0)))]
+    (if (spy req-exist?)
       (select-keys tmpl ALLOWED-KEYS-FOR-USED-TEMPLATE))
-      tmpl))
+    tmpl))
 
 (defn update-template!
   [tx tmpl]
-  (jdbc/execute! tx
-                 (-> (sql/update :procurement_templates)
-                     (sql/sset (validate-update-attributes tx tmpl))
-                     (sql/where [:= :procurement_templates.id (:id tmpl)])
-                     sql/format)))
+  (spy (jdbc/execute! tx (-> (sql/update :procurement_templates)
+                        (sql/sset (validate-update-attributes tx tmpl))
+                        (sql/where [:= :procurement_templates.id (:id tmpl)])
+                        sql/format))))
 
 (defn delete-template!
   [tx id]
-  (jdbc/execute! tx
-                 (-> (sql/delete-from :procurement_templates)
-                     (sql/where [:= :procurement_templates.id id])
-                     sql/format)))
+  (spy (jdbc/execute! tx (-> (sql/delete-from :procurement_templates)
+                        (sql/where [:= :procurement_templates.id id])
+                        sql/format))))
 
 (defn get-template
   ([context _ value]
    (println ">o> tocheck ??? get-template" value)
 
-   (get-template-by-id (-> context
+   (spy (get-template-by-id (-> context
                            :request
                            :tx)
-                       (or (:value value) ; for RequestFieldTemplate
-                           (:template_id value))))
+                       (or (:value value)                   ; for RequestFieldTemplate
+                           (:template_id value)))))
   ([tx tmpl]
-   (let [where-clause (sql/map->where-clause :procurement_templates tmpl)]
+   (spy (let [where-clause (sql/map->where-clause :procurement_templates tmpl)]
      (-> templates-base-query
          (sql/merge-where where-clause)
          sql/format
          (->> (jdbc/query tx))
-         first))))
+         first)))))
 
 (defn requests-count [{{:keys [tx]} :request} _ {:keys [id]}]
-  (-> (sql/select :%count.*)
+  (spy (-> (sql/select :%count.*)
       (sql/from :procurement_requests)
       (sql/where [:= :template_id id])
       sql/format
       (->> (jdbc/query tx))
       first
-      :count))
+      :count)))
--- server/src/leihs/procurement/resources/templates.clj
+++ server/src/leihs/procurement/resources/templates.clj
@@ -39,12 +39,12 @@
         p (println ">oo> templates::get-templates1b ?broken-base-query?" (-> templates-base-query sql/format))
         p (println ">oo> templates::get-templates1c" (sql/format query))
         ]
-    (->> (spy query)
+    (spy (->> (spy query)
          sql/format
          spy
          (jdbc/query (-> context
-                              :request
-                              :tx)))))
+                         :request
+                         :tx))))))
 
 
 (comment
@@ -93,22 +93,21 @@
 
 (defn get-templates-for-ids
   [tx ids]
-  (-> categories/categories-base-query
+  (spy (-> categories/categories-base-query
       (sql/merge-where [:in :procurement_categories.id ids])
       sql/format
-      (->> (jdbc/query tx))))
+      (->> (jdbc/query tx)))))
 
 (defn delete-templates-not-in-ids!
   [tx ids]
-  (jdbc/execute! tx
-                 (-> (sql/delete-from :procurement_templates)
-                     (sql/where [:not-in :procurement_templates.id ids])
-                     sql/format)))
+  (spy (jdbc/execute! tx (-> (sql/delete-from :procurement_templates)
+                             (sql/where [:not-in :procurement_templates.id ids])
+                             sql/format))))
 
 (defn get-template-id
   [tx tmpl]
-  (or (:id tmpl)
-      (as-> tmpl <> (dissoc <> :id) (template/get-template tx <>) (:id <>))))
+  (or (spy (:id tmpl))
+      (spy (as-> tmpl <> (dissoc <> :id) (template/get-template tx <>) (:id <>)))))
 
 (defn update-templates!
   [context args _]
@@ -121,7 +120,7 @@
            tmpl-ids []]
       (println ">o> templates " tmpl)
       (if (spy tmpl)
-         (do (authorization/authorize-and-apply
+        (do (authorization/authorize-and-apply
               #(if-let [id (:id tmpl)]
                  (if (spy (:to_delete tmpl))
                    (spy (template/delete-template! tx id))
@@ -141,3 +140,25 @@
         (spy (categories/get-categories-for-ids tx cat-ids))
 
         ))))
+
+
+;templates
+;mutation
+;>> result: {"data"=>{"update_templates"=>nil}, "errors"=>[{"message"=>"UnauthorizedException - Not authorized for this query path and arguments.", "locations"=>[{"line"=>2, "column"=>11}], "path"=>["update_templates"], "extensions"=>{"exception"=>"ExceptionInfo", "arguments"=>{"input_data"=>[{"id"=>"f864bfae-fdf7-4e00-a32b-029bb4a40e9f", "article_name"=>"test", "category_id"=>"7881e996-71b8-4061-bb63-bba6fc6aa443", "price_cents"=>100}, {"id"=>"4f43e88a-5b68-46ec-abfa-a19f609cab87", "article_name"=>"test", "category_id"=>"e8a70264-5d35-4f2e-a71b-71e31fbfe421", "price_cents"=>100}]}}}]}
+;>>> data={:article_name=>"tmpl for category A", :category_id=>"7881e996-71b8-4061-bb63-bba6fc6aa443"}
+;>>> Template.find(data)=#<Template:0x0000000114884428>
+;>>> data={:article_name=>"tmpl for category B", :category_id=>"e8a70264-5d35-4f2e-a71b-71e31fbfe421"}
+;>>> Template.find(data)=#<Template:0x000000011487ec08>
+;throws if not inspector of some category
+
+
+;>o> templates  {:id f864bfae-fdf7-4e00-a32b-029bb4a40e9f, :article_name test, :category_id 7881e996-71b8-4061-bb63-bba6fc6aa443, :price_cents 100}
+;2023-12-16T18:09:22.538Z NX-41294 DEBUG [leihs.procurement.resources.templates:122] - tmpl => {:id "f864bfae-fdf7-4e00-a32b-029bb4a40e9f", :article_name "test", :category_id "7881e996-71b8-4061-bb63-bba6fc6aa443", :price_cents 100}
+;2023-12-16T18:09:22.540Z NX-41294 DEBUG [leihs.procurement.resources.templates:125] - (:to_delete tmpl) => nil
+;2023-12-16T18:09:22.541Z NX-41294 DEBUG [leihs.procurement.resources.template:37] - tmpl => {:id "f864bfae-fdf7-4e00-a32b-029bb4a40e9f", :article_name "test", :category_id "7881e996-71b8-4061-bb63-bba6fc6aa443", :price_cents 100}
+;2023-12-16T18:09:22.544Z NX-41294 DEBUG [leihs.procurement.resources.template:35] - (-> (sql/select :%count.*) (sql/from :procurement_requests) (sql/where [:= :template_id (:id (spy tmpl))]) sql/format (->> (jdbc/query tx)) first :count (> 0)) => false
+;2023-12-16T18:09:22.544Z NX-41294 DEBUG [leihs.procurement.resources.template:41] - req-exist? => false
+;2023-12-16T18:09:22.562Z NX-41294 DEBUG [leihs.procurement.resources.template:47] - (jdbc/execute! tx (-> (sql/update :procurement_templates) (sql/sset (validate-update-attributes tx tmpl)) (sql/where [:= :procurement_templates.id (:id tmpl)]) sql/format)) => (1)
+;2023-12-16T18:09:22.563Z NX-41294 DEBUG [leihs.procurement.resources.templates:127] - (template/update-template! tx tmpl) => (1)
+;2023-12-16T18:09:22.564Z NX-41294 DEBUG [leihs.procurement.resources.templates:109] - (:id tmpl) => f864bfae-fdf7-4e00-a32b-029bb4a40e9f
+;2023-12-16T18:09:22.564Z NX-41294 DEBUG [leihs.procurement.resources.templates:?] - (conj tmpl-ids (get-template-id tx tmpl)) => ["f864bfae-fdf7-4e00-a32b-029bb4a40e9f"]
